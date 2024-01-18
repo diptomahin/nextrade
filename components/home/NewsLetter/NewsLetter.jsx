85f7702ce69667a7712baa89c7b98a7be0659a3b
jasaiful <jasaiful@gmail.com>
2024-01-19 01:36:03 +0600

    modify newsletter and added favicon

--- components/home/NewsLetter/NewsLetter.jsx
+++ components/home/NewsLetter/NewsLetter.jsx
@@ -1,22 +1,41 @@
-import { Button } from "@mui/material";
-import TextField from "@mui/material/TextField";
-import React from "react";
+import Button from "../../library/Button/Button.jsx"
+import TextField from '@mui/material/TextField';
+import React from 'react';
 
 const NewsLetter = () => {
-  return (
-    <div className="text-white bg-primary flex justify-between rounded-xl px-10 py-10">
-      <div>
-        <h1>Get Daily Updates</h1>
-        <p>Join now with NexTrade to get the latest news and bonuses</p>
-      </div>
-      <div>
-        <TextField id="standard-basic" label="Email" variant="standard" />
-        <button className="p-3 mx-2 font-bold rounded-xl border-x-2 border-y-2 hover:bg-white hover:text-primary">
-          Subscribe
-        </button>
-      </div>
-    </div>
-  );
+
+    const inputStyle = {
+        color: 'white',
+        borderColor: 'white',
+    };
+
+    const labelStyle = {
+        color: 'white',
+    };
+
+    return (
+        <div className='text-white bg-primary-darkBlue md:flex justify-between rounded-xl px-10 py-10'>
+            <div className='mb-10 md:mb-0'>
+                <h1 className='text-2xl'>Get Daily Updates</h1>
+                <p className='font-light'>Join now with NexTrade to get the latest news and bonuses</p>
+            </div>
+            <div>
+                <TextField
+                    id="standard-basic"
+                    label="Enter your email"
+                    variant="standard"
+                    InputLabelProps={{
+                        style: labelStyle,
+                    }}
+                    InputProps={{
+                        style: inputStyle,
+                    }}
+                />
+                <Button>Subscribe</Button>
+                {/* <button className='p-3 mt-2 mx-2 font-bold rounded-xl border-x-2 border-y-2 hover:bg-white hover:text-primary-darkBlue' >Subscribe</button> */}
+            </div>
+        </div>
+    );
 };
 
 export default NewsLetter;
