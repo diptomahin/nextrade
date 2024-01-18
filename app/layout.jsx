145da9c37de5fac6f4c530a5ae9dd79969a7bcff
mdtajulislammt <mdtajulislammt505@gmail.com>
2024-01-19 04:27:56 +0600

    Footer section implement with tailwinf css and all device responsive and add logo importand layout page abge the px size

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,23 +1,24 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
-import MainLayout from "@/components/Layout/MainLayout";
-import { AppConfig } from "@/config/app.config";
+import { Inter } from 'next/font/google'
+import './globals.css'
+import MainLayout from '@/components/Layout/MainLayout'
+import { AppConfig } from '@/config/app.config'
 
-const inter = Inter({ subsets: ["latin"] });
+
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: AppConfig().app.name,
   description: AppConfig().app.description,
-};
+}
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <MainLayout>
-          <div className="lg:px-14 md:px-10 px-5">{children}</div>
-        </MainLayout>
-      </body>
+      <body className={inter.className} >
+        <div className='lg:px-14 md:px-10 px-5'>
+        <MainLayout>{children}</MainLayout>
+        </div>
+        </body>
     </html>
-  );
+  )
 }
