36c19a85ed0983ab046697ae58752d79bcf92d25
mdnuruzzamannirob <nuruzzamanmd2002@gmail.com>
2024-01-20 19:59:24 +0600

    update button component animation

--- components/home/Banner/Banner.jsx
+++ components/home/Banner/Banner.jsx
@@ -9,32 +9,34 @@ import Link from "next/link";
 
 const Banner = () => {
   return (
-    <div className="min-h-screen">
-      <Container>
-        <div className="min-h-screen flex flex-col-reverse xl:flex-row items-center justify-center gap-5 lg:gap-10 text-white py-28">
-          <div className="flex-1">
+    <div className="min-h-screen bg-[#E9EEF1] ">
+      <Container className="min-h-screen flex flex-col-reverse xl:flex-row items-center justify-center gap-5 lg:gap-10 text-white py-28">
+        <div className="flex-1">
+          {" "}
+          <p className="text-[#3671E9] mb-5">Next Level Trading</p>
+          <h1 className="lg:text-5xl  md:text-3 text-2xl  font-bold lg:mb-8 md:mb-4 mb-4 text-black text-center xl:text-left">
+            Start Trading In Crypto{" "}
+            <span className="text-[#3671E9]">Instant</span>
+          </h1>
+          <p className="font-semibold text-gray-500 text-center xl:text-left">
             {" "}
-            <h1 className="lg:text-5xl  md:text-3 text-2xl  font-bold lg:mb-8 md:mb-4 mb-4 text-primary text-center xl:text-left">
-              Trade Smarter, Grow Faster
-            </h1>
-            <p className="font-semibold text-gray-500 text-center xl:text-left">
-              {" "}
-              Unleash the power of advanced tools and market insights. From
-              beginners to pro traders, we have the resources you need.
-            </p>
-            {/* Tow  Button start and download  */}
-            <div className="flex flex-wrap items-center xl:justify-start justify-center gap-4 lg:my-10 md:my-6 my-5">
-              <Button> Start Trading</Button>
-              <Link href={'register'}><Button> Register</Button></Link>
-            </div>
+            Unleash the power of advanced tools and market insights. From
+            beginners to pro traders, we have the resources you need.
+          </p>
+          {/* Tow  Button start and download  */}
+          <div className="flex flex-wrap items-center xl:justify-start justify-center gap-4 lg:my-10 md:my-6 my-5">
+            <Button> Start Trading</Button>
+            <Link href={"register"}>
+              <Button> Register</Button>
+            </Link>
           </div>
-          <div className="flex-1">
-            <Image
-              alt="Banner"
-              className="md:w-[600px]  md:mb-0  mb-10"
-              src={bannerImg}
-            />
-          </div>
+        </div>
+        <div className="flex-1">
+          <Image
+            alt="Banner"
+            className="md:w-[600px]  md:mb-0  mb-10"
+            src={bannerImg}
+          />
         </div>
       </Container>
     </div>
--- components/library/Button/Button.jsx
+++ components/library/Button/Button.jsx
@@ -2,18 +2,29 @@
 
 import cn from "@/utils/cn";
 import "./button.css";
+import { RiArrowRightDoubleLine } from "react-icons/ri";
 
 const Button = ({ children, className, ...restProps }) => {
   return (
     <button
       className={cn(
-        "scroll-text px-10 rounded-full font-medium md:font-semibold font-dm text-white h-8 md:h-10 text-sm md:text-base bg-primary",
+        "scroll-text px-10 rounded-full font-dm font-medium text-white h-8 md:h-10 text-sm md:text-base bg-gradient-to-r  from-[#239FFE] to-[#0272E5] ",
         className
       )}
       {...restProps}
     >
-      <span className="text1 whitespace-nowrap">{children}</span>
-      <span className="text2 whitespace-nowrap">{children}</span>
+      <span className="text1 flex items-center gap-1">
+        <span className="whitespace-nowrap">{children} </span>
+        <span className="icon1">
+          <RiArrowRightDoubleLine />
+        </span>
+      </span>
+      <span className="text2 flex items-center gap-1">
+        <span className="whitespace-nowrap">{children} </span>
+        <span className="icon2">
+          <RiArrowRightDoubleLine />
+        </span>
+      </span>
     </button>
   );
 };
