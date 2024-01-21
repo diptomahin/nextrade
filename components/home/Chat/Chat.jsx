9462c79713dc3ed169ad412367684339cafdf951
Ariful Islam <hello.mrariful@gmail.com>
2024-01-21 15:56:01 +0600

    chat

---
+++ components/home/Chat/Chat.jsx
@@ -0,0 +1,16 @@
+"use client";
+import React from 'react';
+import MessengerCustomerChat from "react-messenger-customer-chat";
+
+const Chat = () => {
+    return (
+        <div className="App">
+        <MessengerCustomerChat
+          pageId="109810151659600"
+          appId="727980532201110"
+        />
+      </div>
+    );
+};
+
+export default Chat;
