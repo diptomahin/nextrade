a429e3060444d5bf32d36c8ab6c98a84ac51fbce
jasaiful <jasaiful@gmail.com>
2024-01-23 00:52:00 +0600

    added card

--- app/helpCenter/page.jsx
+++ app/helpCenter/page.jsx
@@ -4,6 +4,7 @@ import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
+import NotStartedOutlinedIcon from '@mui/icons-material/NotStartedOutlined';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import Button from "@/components/library/Button/Button";
@@ -18,17 +19,11 @@ const page = () => {
                <Typography variant="h2" mt={5} mb={3} fontWeight="bold" className='text-primary' sx={{ textAlign: "center", fontSize: ["28px", "28px", "52px"] }}>Welcome to our <br /> Help Center</Typography>
                <Typography variant="h6" gutterBottom maxWidth={700} marginX="auto" sx={{ textAlign: "center", fontSize: ["16px", "20px", "24px"] }}>Welcome to our help center! Need assistance? <br /> We&apos;re here for you. Explore our resources or contact our support team. Your satisfaction is our priority!</Typography>
                <div>
-                    <div>
+                    <div className="grid xl:grid-cols-3 lg:grid-cols-2 grid-cols-1 lg:gap-6 xl:gap-10 gap-4 text-center mx-auto justify-center item-center mt-10">
                          {/* cart 1  */}
-                         <Card className="py-14 px-2 rounded-lg border border-gray-100">
-                              <div className="w-24 h-fit text-center mx-auto bg-[#22356b] rounded-full">
-                                   {/* <Image
-                                        src={Icon1}
-                                        alt="Trad Icon"
-                                        width={400}
-                                        height={500}
-                                        placeholder="blur"
-                                   /> */}
+                         <Card className="p-5 bg-white rounded-2xl border border-slate-200">
+                              <div className=" bg-blue-200 text-center mx-auto rounded-xl w-16 h-16  flex justify-center items-center lg:mb-0 mb-3 ">
+                                   <NotStartedOutlinedIcon className=" text-2xl text-primary " />
                               </div>
                               <CardContent>
                                    <Typography
@@ -39,10 +34,12 @@ const page = () => {
                                    >
                                         Getting started
                                    </Typography>
-                                   <Typography variant="body2" color="text.secondary" className="font-semibold text-gray-400">
-                                   Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium tempore ex commodi eligendi expedita soluta rerum doloribus minus adipisci. Doloribus velit quidem eius labore rerum repellendus deserunt porro optio totam?
+                                   <Typography variant="body2" color="text.secondary" className="text-gray-500">
+                                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium tempore ex commodi eligendi expedita soluta rerum doloribus minus adipisci. Doloribus velit quidem eius labore rerum repellendus deserunt porro optio totam?
                                    </Typography>
-                                   <Button>Browse questions</Button>
+                                   <div className='pt-5'>
+                                        <Button>Browse questions</Button>
+                                   </div>
                               </CardContent>
                          </Card>
                     </div>
