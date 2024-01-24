fdfb3389f5758695ed9b2984fd1711ce460e62c3
Mahin Ahmed <mahindipto18@gmail.com>
2024-01-25 04:24:00 +0600

    added some more coin data

--- app/dashboard/trading/page.jsx
+++ app/dashboard/trading/page.jsx
@@ -11,6 +11,8 @@ import logo from "../../../assets/nextrade-logo.png";
 import imageBTC from "../../../assets/coinImages/bitcoin.png"
 import imageETH from "../../../assets/coinImages/ethereum.png"
 import imageLTC from "../../../assets/coinImages/ltc.png"
+import imageQTUM from "../../../assets/coinImages/QTUM.png"
+import imageDOGE from "../../../assets/coinImages/DOGE.png"
 // material imports
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -20,18 +22,18 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import Image from 'next/image';
+import Button from '@/components/library/Button/Button';
 
 
 const Trading = () => {
 
-    const [currentBTCPrice, setCurrentBTCPrice] = useState(0);
-    const [currentLTCPrice, setCurrentLTCPrice] = useState(0);
-    const [currentETHPrice, setCurrentETHPrice] = useState(0);
+    const [BTCPrice, setBTCPrice] = useState(0);
+    const [LTCPrice, setLTCPrice] = useState(0);
+    const [ETHPrice, setETHPrice] = useState(0);
+    const [QTUMPrice, setQTUMPrice] = useState(0);
+    const [DOGEPrice, setDOGEPrice] = useState(0);
     
-    const buyingBTCPrice = parseInt('40020')
-    const buyingETHPrice = parseInt('2050')
-    const buyingLTCPrice = parseInt('60')
-
+ 
 
     React.useEffect(() => {
         // Create a WebSocket connection
@@ -40,18 +42,26 @@ const Trading = () => {
         // Event listener for incoming messages
         socket.addEventListener('message', (event) => {
             const data = JSON.parse(event.data);
-            // console.log(data)
+            console.log(data)
 
             // Find and update prices for BTC, LTC, and ETH
             data.forEach((ticker) => {
                 const symbol = ticker.s;
 
                 if (symbol === 'BTCUSDT') {
-                    setCurrentBTCPrice(parseFloat(ticker.c).toFixed(2));
-                } else if (symbol === 'LTCUSDT') {
-                    setCurrentLTCPrice(parseFloat(ticker.c).toFixed(2));
-                } else if (symbol === 'ETHUSDT') {
-                    setCurrentETHPrice(parseFloat(ticker.c).toFixed(2));
+                    setBTCPrice(parseFloat(ticker.c).toFixed(2));
+                }
+                 else if (symbol === 'LTCUSDT') {
+                    setLTCPrice(parseFloat(ticker.c).toFixed(2));
+                } 
+                else if (symbol === 'ETHUSDT') {
+                    setETHPrice(parseFloat(ticker.c).toFixed(2));
+                }
+                else if (symbol === 'QTUMUSDT') {
+                    setQTUMPrice(parseFloat(ticker.c).toFixed(2));
+                }
+                else if (symbol === 'DOGEUSDT') {
+                    setDOGEPrice(parseFloat(ticker.c).toFixed(2));
                 }
             });
         });
@@ -67,9 +77,11 @@ const Trading = () => {
     }
 
     const assets = [
-        createData('Bitcoin (BTC)', currentBTCPrice, imageBTC),
-        createData('Ethereum (ETC)', currentETHPrice, imageETH),
-        createData('LiteCoin (LTC)', currentLTCPrice, imageLTC),
+        createData('Bitcoin (BTC)', BTCPrice, imageBTC),
+        createData('Ethereum (ETC)', ETHPrice, imageETH),
+        createData('LiteCoin (LTC)', LTCPrice, imageLTC),
+        createData('QTUM coin', QTUMPrice, imageQTUM),
+        createData('DOGE coin', DOGEPrice, imageDOGE),
     ];
 
 
@@ -82,16 +94,17 @@ const Trading = () => {
                 <h2 className=" text-4xl font-bold text-primary mb-3 font-sans">Start Trading. . . . . </h2>
                 <TableContainer component={Paper}>
                     <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                        <TableHead className="bg-primary ">
+                        <TableHead className="bg-primary mx-auto">
                             <TableRow className='text-center' >
                                 <TableCell sx={{color:"white"}}  className="font-semibold ">Icon</TableCell>
                                 <TableCell  sx={{color:"white"}}  className=" font-semibold">Crypto</TableCell>
                                 <TableCell sx={{color:"white"}}  className="font-semibold">Current Price</TableCell>
+                                <TableCell sx={{color:"white"}}  className="font-semibold"></TableCell>
             
                             </TableRow>
                         </TableHead>
                         <TableBody >
-                            {assets.map((asset) => (
+                        {assets.map((asset) => (
                                 <TableRow
                                     key={asset.name}
                                     sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
@@ -105,8 +118,8 @@ const Trading = () => {
                                     <TableCell >
                                         <p className={`text-lg font-semibold`}>{asset.price}</p>
                                     </TableCell>
-                                    <TableCell>
-                                        
+                                    <TableCell >
+                                        <Button>Trade</Button>
                                     </TableCell>
                                 </TableRow>
                             ))}
