93ff7f3c3abdfffa42b4d3f6e4bb526f5fb39d24
jasaiful <jasaiful@gmail.com>
2024-01-24 23:13:45 +0600

    added 3 part

--- app/dashboard/wallet/page.jsx
+++ app/dashboard/wallet/page.jsx
@@ -1,20 +1,31 @@
 "use client"
 
+import Button from "@/components/library/Button/Button";
 import Container from "@/components/library/Container";
+import BorderColorIcon from '@mui/icons-material/BorderColor';
+import AddIcon from '@mui/icons-material/Add';
+
 
 const Wallet = () => {
     return (
-        <Container>
-            <div>
-                <div>
-                    <h1 className='text-2xl font-bold'>Wallet</h1>
+        <Container className="flex justify-between gap-5 w-full p-2 bg-[#e9eef1]">
+            <div className="w-9/12 flex flex-col gap-5">
+                <div className="flex p-2 border bg-white rounded-lg justify-between">
+                    <div>
+                        <h1 className='text-xl font-bold'>Wallet</h1>
+                        <p className="text-sm">Update 16/02/2022 at 02:30PM</p>
+                    </div>
+                    <div>
+                        <Button> <BorderColorIcon className=" text-white"/> Edit</Button>
+                        <Button className="ml-5"> <AddIcon className=" text-white"/> Add New Wallet</Button>
+                    </div>
                 </div>
-                <div>
-                    <h1 className='text-2xl font-bold'>Transaction History</h1>
+                <div className="border p-2 bg-white rounded-lg">
+                    <h1 className='text-xl font-bold'>Transaction History</h1>
                 </div>
             </div>
-            <div>
-                <h1 className='text-2xl font-bold'>Select Currency & Payment</h1>
+            <div className="w-4/12 border p-2 bg-white rounded-lg">
+                <h1 className='text-xl text-center font-bold'>Select Currency & Payment</h1>
             </div>
         </Container>
     );
