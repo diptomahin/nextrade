52a211497e23842478d851fee7b8a2291f694247
jasaiful <jasaiful@gmail.com>
2024-01-24 17:00:15 +0600

    created initial wallet page

--- app/dashboard/wallet/page.jsx
+++ app/dashboard/wallet/page.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+"use client"
+
+import Container from "@/components/library/Container";
 
 const Wallet = () => {
     return (
-        <div>
-            <h1 className='text-2xl font-bold'>This is Wallet page</h1>
-        </div>
+        <Container>
+            <div>
+                <div>
+                    <h1 className='text-2xl font-bold'>Wallet</h1>
+                </div>
+                <div>
+                    <h1 className='text-2xl font-bold'>Transaction History</h1>
+                </div>
+            </div>
+            <div>
+                <h1 className='text-2xl font-bold'>Select Currency & Payment</h1>
+            </div>
+        </Container>
     );
 };
 
