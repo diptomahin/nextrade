42ec21d56a9e29c4377eed9455327e00f76cc608
mdtajulislammt <mdtajulislammt505@gmail.com>
2024-01-26 14:30:59 +0600

    bug/Fixed/portfolio page

--- app/dashboard/portfolio/page.jsx
+++ app/dashboard/portfolio/page.jsx
@@ -14,8 +14,8 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import useAllUsers from "@/app/hooks/useAllUsers";
 
-import useAllUsers from "@/hooks/useAllUsers";
 
 const Portfolio = () => {
   const [allUsers] = useAllUsers();
