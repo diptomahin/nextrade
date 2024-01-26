a0cfccfa9d0c9772bde13ba38fb26adcc2538949
srs-sieam <srssieam@gmail.com>
2024-01-26 11:36:29 +0600

    replace server api with deployed api

--- app/hooks/useAxiosPublic.jsx
+++ app/hooks/useAxiosPublic.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const axiosPublic = axios.create({
-    baseURL: "http://localhost:5000/v1/api"
+    baseURL: "https://nex-trade-server.vercel.app/v1/api"
 })
 const useAxiosPublic = () => {
     return axiosPublic;
