81fb1b851afce0b59a3d6a0353e0365fe04a0c16
mahindipto <mahindipto18@gmail.com>
2024-01-30 19:11:31 +0600

    added style to profile input fileds and added profile image

--- app/dashboard/profile/page.jsx
+++ app/dashboard/profile/page.jsx
@@ -2,10 +2,11 @@
 import React from "react";
 import { AuthContext } from "@/provider/AuthProvider";
 import { useContext } from "react";
+import { Avatar } from '@mui/material'
 
 const ProfilePage = () => {
   const { user, updateUserProfile } = useContext(AuthContext);
-
+  const profilePic = user.photoURL;
   const updateProfileInfo = (e) => {
     e.preventDefault();
     const form = new FormData(e.currentTarget);
@@ -18,7 +19,13 @@ const ProfilePage = () => {
   };
 
   return (
-    <div>
+    <div className="w-9/12 mx-auto">
+       <Avatar
+        alt="profile Image" 
+        src={user.photoURL}
+        className="my-10"
+        sx={{ width: 100, height: 100 ,}}
+         />
       <form onSubmit={updateProfileInfo}>
         <div className="flex gap-2">
           <input
@@ -26,21 +33,20 @@ const ProfilePage = () => {
             defaultValue={user.displayName}
             size="lg"
             name="name"
-            placeholder="name@mail.com"
-            className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+            placeholder="User Name"
+            className=" w-52 border-primary border-x-2 border-y-2 p-3 rounded-lg text-primary my-3 font-semibold "
           />
         </div>
-
         <div className="flex gap-2">
           <input
             type="url"
             size="lg"
             name="photo"
             placeholder="Enter Photo URL"
-            className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+            className="w-52  border-primary border-x-2 border-y-2 p-3 rounded-lg text-primary my-3 font-semibold"
           />
         </div>
-        <button className="">
+        <button className="p-3 rounded-lg font-semibold hover:text-primary hover:bg-white hover:border-2 hover:border-primary bg-primary text-white">
           Submit
         </button>
       </form>
