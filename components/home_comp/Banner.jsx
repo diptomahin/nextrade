81d81a42f17db5e21f1b22d119811a3f9625e0b9
mdtajulislammt <mdtajulislammt505@gmail.com>
2024-01-31 01:40:54 +0600

    home/Banner

--- components/home_comp/Banner.jsx
+++ components/home_comp/Banner.jsx
@@ -2,12 +2,23 @@
 import Button from "@/components/library/buttons/root_button/RootButton";
 import Container from "@/components/library/Container";
 import Magnetic from "@/components/library/Magnetic";
+import Image from "next/image";
 import { TickerTape } from "react-ts-tradingview-widgets";
+
+//banner image impost asset
+import bannerImg from '../../assets/Trading-PNG-Photo.png'
+
 
 const Banner = () => {
   return (
-    <Container className="relative min-h-[100vh] flex flex-col items-center justify-center gap-8 py-32">
-      <div className="text-5xl sm:text-6xl md:text-8xl xl:text-9xl font-extrabold text-secondary md:mb-5 mb-4 flex flex-wrap items-center justify-center">
+    <Container className="relative min-h-[100vh] flex flex-col items-center justify-center gap-8 py-32 ">
+      <div className=" lg:flex flex-row-reverse items-center justify-between gap-10">
+        
+        <div className="">
+          <Image src={bannerImg} alt=""/>
+        </div>
+        <div>
+          <div className=" text-3xl sm:text-3xl md:text-3xl lg:text-2xl xl:text-5xl font-extrabold text-secondary md:mb-5 mb-4 flex flex-wrap items-center  ">
         <Magnetic>
           <h1 className="text-primary">Innovation</h1>
         </Magnetic>
@@ -21,13 +32,16 @@ const Banner = () => {
         </Magnetic>
         .
       </div>
-      <p className="xl:w-11/12 mx-auto text-lg lg:text-2xl xl:text-3xl font-medium text-secondary text-justify md:text-center">
+      <p className=" text-sm md:text-md lg:text-lg xl:text-xl mb-8 font-medium text-secondary ">
         {" "}
         Join world&apos;s biggest & trusted Exchange. Trade in Bitcoin,
         Ethereum, LiteCoin, DOGE coin and many more currencies.
       </p>
 
       <Button>Trade Now</Button>
+      </div>
+      </div>
+      
 
       <div className="absolute bottom-5 w-full">
         <TickerTape displayMode="adaptive"></TickerTape>
