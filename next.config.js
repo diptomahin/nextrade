4db4ea39687c4ff83986d13ff0c7947e1fab9baf
srs-sieam <srssieam@gmail.com>
2024-02-01 08:49:37 +0600

    get coin image from coingecko.com

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+    images: {
+      domains: ['assets.coingecko.com'],
+    },
+  }
