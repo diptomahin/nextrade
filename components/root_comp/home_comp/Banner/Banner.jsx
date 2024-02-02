649e53baf5b42c3914d57470497ea5f5e7aa4806
MD Tajul Islam <mdtajulislam50599@gmail.com>
2024-02-02 13:01:50 +0600

    Update Banner

--- components/root_comp/home_comp/Banner/Banner.jsx
+++ components/root_comp/home_comp/Banner/Banner.jsx
@@ -6,14 +6,14 @@ import Image from "next/image";
 import { TickerTape } from "react-ts-tradingview-widgets";
 
 //banner image impost asset
-// import bannerImg from "../../../assets/Trading-PNG-Photo.png";
-import banner from "../../../../assets/Banner/banner.png";
+import bannerImg from "../../../assets/Trading-PNG-Photo.png";
+import banner from '../../../assets/Banner/banner.png'
 
 //framer motion
 import { motion } from "framer-motion";
 
 // variants
-import { fadeIn } from "../../../utils/variants";
+import { fadeIn } from "../../utils/variants";
 
 const Banner = () => {
   return (
@@ -25,8 +25,10 @@ const Banner = () => {
       className="relative h-[100vh]"
       // style={{ backgroundImage: `url(${banner})`, backgroundSize: "cover", backgroundPosition: "center" }}
     >
-      <Image src={banner} alt="" className=" w-full h-full object-cover" />
-      <div className="absolute w-full h-full top-0 px-10 py-32 bg-gradient-to-r from-[#0000002a] to-[#0000005d]">
+      <Image src={banner} alt="" className=" w-full h-full object-cover"/>
+      <div 
+      
+      className="absolute w-full h-full top-0 px-10 py-32 bg-gradient-to-r from-[#0000007e] to-[#000000c0]">
         <motion.div
           variants={fadeIn("up", 0.2)}
           initial="hidden"
@@ -46,13 +48,13 @@ const Banner = () => {
             </motion.div>
           </Magnetic> */}
 
-          <div className=" text-center w-full mx-auto  mt-24">
+          <div className=" text-center w-full mx-auto    xl:mt-24">
             <motion.h1
               variants={fadeIn("down", 0.1)}
               initial="hidden"
               whileInView={"show"}
               viewport={{ once: false, amount: 0.1 }}
-              className=" text-3xl sm:text-3xl md:text-3xl lg:text-2xl xl:text-6xl font-extrabold text-secondary md:mb-5 mb-4   text-center"
+              className=" text-xl  sm:text-3xl md:text-3xl lg:text-2xl xl:text-6xl 3xl:text-9xl font-extrabold text-secondary md:mb-5 2xl:mb-5   text-center flex flex-wrap justify-center"
             >
               <Magnetic>
                 <span className="text-white">Innovation</span>
@@ -72,12 +74,11 @@ const Banner = () => {
               initial="hidden"
               whileInView={"show"}
               viewport={{ once: false, amount: 0.1 }}
-              className=" text-sm md:text-md lg:text-lg xl:text-xl mb-8 font-medium text-[#e4e2e2] "
+              className=" text-sm md:text-md lg:text-lg xl:text-xl 3xl:text-4xl mb-8 font-medium text-[#e4e2e2] "
             >
               {" "}
-              Join world&apos;s biggest & trusted Exchange. Trade in Bitcoin,
-              Ethereum, LiteCoin,
-              <br /> DOGE coin and many more currencies.
+              Join world&apos;s biggest & trusted Exchange. Trade  in Bitcoin,
+              Ethereum, LiteCoin,<br /> DOGE coin and many more currencies.
             </motion.p>
             <motion.div
               variants={fadeIn("down", 0.4)}
@@ -89,11 +90,12 @@ const Banner = () => {
             </motion.div>
           </div>
         </motion.div>
-        <Container>
-          <div className="absolute bottom-2 w-full mx-auto left-0  ">
-            <TickerTape displayMode="adaptive"></TickerTape>
-          </div>
-        </Container>
+          <Container>
+
+        <div className="absolute bottom-0 w-full mx-auto left-0  ">
+          <TickerTape displayMode="adaptive"></TickerTape>
+        </div>
+          </Container>
       </div>
     </motion.div>
   );
