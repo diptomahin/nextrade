b56ff45b783bea0b20fcf536c7a1c0f0c77daae3
srs-sieam <srssieam@gmail.com>
2024-02-05 22:15:08 +0600

    fetch watchlist data

--- app/dashboard/watchlist/page.jsx
+++ app/dashboard/watchlist/page.jsx
@@ -1,9 +1,20 @@
+"use client"
+import useAuth from '@/hooks/useAuth';
+import useSecureFetch from '@/hooks/useSecureFetch';
 import React from 'react';
 
 const Watchlist = () => {
+    const { user, loading } = useAuth();
+    // console.log(user)
+    const { data: watchlistData=[], isPending, isLoading, refetch } = useSecureFetch(
+        `/watchlist?email=${user.email}`,
+        ["watchlist",user.email]
+    );
+    console.log(watchlistData)
     return (
         <div>
             <h1 className='text-2xl font-bold'>This page under Construction </h1>
+            <h1 className='text-2xl font-bold'>total item {watchlistData.length} </h1>
         </div>
     );
 };
