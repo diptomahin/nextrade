e215b848bfac6e44f998c0a0d454ab69d3766650
srs-sieam <srssieam@gmail.com>
2024-02-06 19:45:48 +0600

    improve style in market page

--- app/dashboard/market/page.jsx
+++ app/dashboard/market/page.jsx
@@ -7,6 +7,8 @@ import DashboardButton from "@/components/library/buttons/DashButton";
 import Link from "next/link";
 import Marquee from "react-fast-marquee";
 import { Stack } from "@mui/material";
+import TrendingUpIcon from '@mui/icons-material/TrendingUp';
+import TrendingDownIcon from '@mui/icons-material/TrendingDown';
 
 //image imports
 import imageBTC from "../../../assets/coinImages/bitcoin.png";
@@ -54,9 +56,9 @@ const MarketPage = () => {
           return createData(
             asset.name,
             asset.key,
-            parseFloat(ticker.c).toFixed(2),
+            parseFloat(ticker.c).toFixed(3),
             asset.icon,
-            parseFloat(ticker.p).toFixed(3),
+            parseFloat(ticker.p).toFixed(2),
             parseFloat(ticker.h).toFixed(2),
             parseFloat(ticker.l).toFixed(2)
           );
@@ -77,127 +79,135 @@ const MarketPage = () => {
   return (
     <div>
       {/* Table boat  */}
-      <div>
+      <div className="bg-white p-4 rounded-xl">
         <h2 className=" text-xl font-semibold mb-3">Market Coins</h2>
-        <Stack my={5}>
+        <p>Choose from a wide range of trade options with hundreds of different instruments available.</p>
+        <Stack mt={4}>
           <Marquee direction='right'>
             {assets.map((asset) => (
-              <Stack key={asset.name} sx={{ bgcolor: '#ffff', px: 2, py: 1, borderRadius: '9 px' }} justifyContent="space-between" alignItems="center" marginX={4} gap={2} flexDirection="row">
+              <Stack className="bg-sky-100 rounded-lg h-24" key={asset.name} sx={{ px: 2, py: 1 }} justifyContent="space-between" alignItems="center" marginX={4} gap={3} flexDirection="row">
                 <Stack>
-                  <Image height={80} width={80} src={asset.icon} alt='coin lgog'></Image>
+                  <Stack flexDirection="row" gap={1} alignItems="center">
+                    <Image width={45} height={45} src={asset.icon} alt='coin lgog'></Image>
+                    <p>{asset.name}</p>
+                  </Stack>
+                  <Stack flexDirection="row" gap={2}>
+                  <p>${asset.price}</p>
+                  <p className={` ${asset.changePrice < 0 ? "text-red-700" : "text-green-700"}`}>{asset.changePrice}% {asset.changePrice < 0 ? <TrendingDownIcon />:<TrendingUpIcon />}</p>
+                  </Stack>
                 </Stack>
                 <Stack>
-                  <p className="">Current Price:{asset.price}</p>
-                  <p>Heigh Price: <span className="text-green-700">{asset.heighPrice}</span></p>
-                  <p>Low Price: <span className="text-red-700">{asset.lowPrice}</span></p>
+                  <p>Heigh Price: <span className="text-green-700">${asset.heighPrice}</span></p>
+                  <p>Low Price: <span className="text-red-700">${asset.lowPrice}</span></p>
                 </Stack>
               </Stack>
             ))}
           </Marquee>
         </Stack>
-
-        <Stack flexDirection="row" gap={2} marginY={2}>
-          <FormControl sx={{ width: 200 }}>
-            <InputLabel id="demo-simple-select-label">Category</InputLabel>
-            <Select
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
-              value={category}
-              label="Category"
-              onChange={handleCategoryChange}
-            >
-              <MenuItem value={10} >Crypto</MenuItem>
-              <MenuItem value={20} >Currency</MenuItem>
-              <MenuItem value={30} >Stocks</MenuItem>
-            </Select>
-          </FormControl>
-          <FormControl sx={{ width: 200 }}>
-            <InputLabel id="demo-simple-select-label">Sort by</InputLabel>
-            <Select
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
-              value={category}
-              label="Sort by"
-              onChange={handleCategoryChange}
-            >
-              <MenuItem value={10} >Current Price</MenuItem>
-              <MenuItem value={20} >24h Heigh Price</MenuItem>
-              <MenuItem value={30} >24h Low Price</MenuItem>
-            </Select>
-          </FormControl>
-        </Stack>
-
-        <div className="flex flex-col xl:flex-row gap-5">
-          <div className="w-full xl:w-3/4">
-            <TableContainer
-              component={Paper}
-              sx={{
-                border: "1px solid rgba(0, 0, 0, 0.1)",
-              }}
-            >
-              <Table aria-label="simple table">
-                <TableHead className="mx-auto bg-primary">
-                  <TableRow className="text-center">
-                    <TableCell sx={{ fontWeight: 700, color: "white" }}>No.</TableCell>
-                    <TableCell sx={{ fontWeight: 700, color: "white" }}>Coin Name</TableCell>
-                    <TableCell sx={{ fontWeight: 700, color: "white" }}>Current Price</TableCell>
-                    <TableCell sx={{ fontWeight: 700, color: "white" }}>24%</TableCell>
-                    <TableCell sx={{ fontWeight: 700, color: "white" }}>24h Heigh Price</TableCell>
-                    <TableCell sx={{ fontWeight: 700, color: "white" }}>24h Low Price</TableCell>
-                    <TableCell sx={{ fontWeight: 700, color: "white" }}>Option</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {assets.map((asset, idx) => (
-                    <TableRow
-                      key={asset.name}
-                      sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                    >
-                      <TableCell component="th" scope="row">
-                        {idx + 1}
-                      </TableCell>
-                      <TableCell>
-                        <div className="flex items-center gap-2">
-                          <Image
-                            width={40}
-                            height={40}
-                            src={asset.icon}
-                            alt="coin-icon"
-                          />
-                          <p className={`font-semibold`}>{asset.name}</p>
-                          <span className="bg-sky-100 px-1 py-[2px] rounded text-primary text-xs">{asset.key.slice(0, -4)}</span>
-                        </div>
-                      </TableCell>
-                      <TableCell>
-                        <p className={` font-semibold`}>$ {asset.price}</p>
-                      </TableCell>
-                      <TableCell>
-                        <p className={` font-semibold ${asset.changePrice < 0 ? "text-red-700" : "text-green-700"}`}>$ {asset.changePrice}</p>
-                      </TableCell>
-                      <TableCell>
-                        <p className={` font-semibold text-green-700`}>$ {asset.heighPrice}</p>
-                      </TableCell>
-                      <TableCell>
-                        <p className={` font-semibold text-red-700`}>$ {asset.lowPrice}</p>
-                      </TableCell>
-                      <TableCell>
-                        <DashboardButton
-                          className="font-semibold normal-case"
-                        >
-                          <Link href={`/dashboard/market/${asset.key}`}>
-                            Explore
-                          </Link>
-                        </DashboardButton>
-                      </TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </TableContainer>
-          </div>
-          <SideWatchlist assets={assets}></SideWatchlist>
-        </div>
       </div>
+
+      <Stack flexDirection="row" gap={2} marginY={2}>
+        <FormControl sx={{ width: 200 }}>
+          <InputLabel id="demo-simple-select-label">Category</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={category}
+            label="Category"
+            onChange={handleCategoryChange}
+          >
+            <MenuItem value={10} >Crypto</MenuItem>
+            <MenuItem value={20} >Currency</MenuItem>
+            <MenuItem value={30} >Stocks</MenuItem>
+          </Select>
+        </FormControl>
+        <FormControl sx={{ width: 200 }}>
+          <InputLabel id="demo-simple-select-label">Sort by</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={category}
+            label="Sort by"
+            onChange={handleCategoryChange}
+          >
+            <MenuItem value={10} >Current Price</MenuItem>
+            <MenuItem value={20} >24h Heigh Price</MenuItem>
+            <MenuItem value={30} >24h Low Price</MenuItem>
+          </Select>
+        </FormControl>
+      </Stack>
+
+      <div className="flex flex-col xl:flex-row gap-5">
+        <div className="w-full xl:w-3/4">
+          <TableContainer
+            component={Paper}
+            sx={{
+              border: "1px solid rgba(0, 0, 0, 0.1)",
+            }}
+          >
+            <Table aria-label="simple table">
+              <TableHead className="mx-auto bg-primary">
+                <TableRow className="text-center">
+                  <TableCell sx={{ fontWeight: 700, color: "white" }}>No.</TableCell>
+                  <TableCell sx={{ fontWeight: 700, color: "white" }}>Coin Name</TableCell>
+                  <TableCell sx={{ fontWeight: 700, color: "white" }}>Current Price</TableCell>
+                  <TableCell sx={{ fontWeight: 700, color: "white" }}>24%</TableCell>
+                  <TableCell sx={{ fontWeight: 700, color: "white" }}>24h Heigh Price</TableCell>
+                  <TableCell sx={{ fontWeight: 700, color: "white" }}>24h Low Price</TableCell>
+                  <TableCell sx={{ fontWeight: 700, color: "white" }}>Option</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {assets.map((asset, idx) => (
+                  <TableRow
+                    key={asset.name}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <TableCell component="th" scope="row">
+                      {idx + 1}
+                    </TableCell>
+                    <TableCell>
+                      <div className="flex items-center gap-2">
+                        <Image
+                          width={40}
+                          height={40}
+                          src={asset.icon}
+                          alt="coin-icon"
+                        />
+                        <p className={`font-semibold`}>{asset.name}</p>
+                        <span className="bg-sky-100 px-1 py-[2px] rounded text-primary text-xs">{asset.key.slice(0, -4)}</span>
+                      </div>
+                    </TableCell>
+                    <TableCell>
+                      <p className={` font-semibold`}>$ {parseFloat(asset.price).toFixed(2)}</p>
+                    </TableCell>
+                    <TableCell>
+                      <p className={` font-semibold ${asset.changePrice < 0 ? "text-red-700" : "text-green-700"}`}>{asset.changePrice}% {asset.changePrice < 0 ? <TrendingDownIcon />:<TrendingUpIcon />}</p>
+                    </TableCell>
+                    <TableCell>
+                      <p className={` font-semibold text-green-700`}>$ {asset.heighPrice}</p>
+                    </TableCell>
+                    <TableCell>
+                      <p className={` font-semibold text-red-700`}>$ {asset.lowPrice}</p>
+                    </TableCell>
+                    <TableCell>
+                      <DashboardButton
+                        className="font-semibold normal-case"
+                      >
+                        <Link href={`/dashboard/market/${asset.key}`}>
+                          Explore
+                        </Link>
+                      </DashboardButton>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </div>
+        <SideWatchlist assets={assets}></SideWatchlist>
+      </div>
+
     </div>
   );
 };
