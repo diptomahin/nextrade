9769cd8fbe7ec2751c9f0fe3504d6e23e20af010
mahindipto <mahindipto18@gmail.com>
2024-02-06 21:27:07 +0600

    fixed some bugs for trading

--- app/dashboard/trading/page.jsx
+++ app/dashboard/trading/page.jsx
@@ -3,12 +3,7 @@
 import React from "react";
 import { useState } from "react";
 
-//image imports
-import imageBTC from "../../../assets/coinImages/bitcoin.png";
-import imageETH from "../../../assets/coinImages/ethereum.png";
-import imageLTC from "../../../assets/coinImages/ltc.png";
-import imageQTUM from "../../../assets/coinImages/QTUM.png";
-import imageDOGE from "../../../assets/coinImages/DOGE.png";
+
 // material imports
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
@@ -140,60 +135,9 @@ const Trading = () => {
   }
 
   return (
-    <div>
-      {/* Table boat  */}
-      <div>
-        <h2 className=" text-xl font-semibold mb-3">Start Trading...</h2>
-        <TableContainer
-          component={Paper}
-          sx={{
-            border: "1px solid rgba(0, 0, 0, 0.1)",
-          }}
-        >
-          <Table sx={{ minWidth: 650 }} aria-label="simple table">
-            <TableHead className="mx-auto">
-              <TableRow className="text-center">
-                <TableCell className="font-semibold ">Icon</TableCell>
-                <TableCell className=" font-semibold">Crypto</TableCell>
-                <TableCell className="font-semibold">Current Price</TableCell>
-                <TableCell className="font-semibold">Trade Option</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {assets.map((asset) => (
-                <TableRow
-                  key={asset.name}
-                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                >
-                  <TableCell component="th" scope="row">
-                    <Image
-                      width={100}
-                      height={100}
-                      src={asset.icon}
-                      alt="coin-icon"
-                    />
-                  </TableCell>
-                  <TableCell>
-                    <p className={`text-lg`}>{asset.name}</p>
-                  </TableCell>
-                  <TableCell>
-                    <p className={`text-lg font-semibold`}>{asset.price}</p>
-                  </TableCell>
-                  <TableCell>
-                    <DashboardButton
-                      onClick={() => handleBuyCoin(asset)}
-                      className="font-semibold normal-case"
-                    >
-                      Trade
-                    </DashboardButton>
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </div>
-    </div>
+  <div>
+    <h1>Trading page</h1>
+  </div>
   );
 };
 
