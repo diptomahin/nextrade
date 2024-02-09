2cf384ed6035201ae8c17f8578572873ac5d3e52
MD Tajul Islam <mdtajulislam50599@gmail.com>
2024-02-09 17:29:05 +0600

    Update PortfolioAssetChart

--- components/traders_comp/portfolio/PortfolioAssetChart.jsx
+++ components/traders_comp/portfolio/PortfolioAssetChart.jsx
@@ -1,48 +1,39 @@
-"use client";
-
-import React, { useState, useEffect } from "react";
+import React from "react";
 import {
   Tooltip,
   ComposedChart,
-  Area,
   Bar,
   CartesianGrid,
   Legend,
-  Line,
   XAxis,
   YAxis,
+  Area,
+  Line,
 } from "recharts";
 
-const PortfolioAssetChart = ({ allUsers }) => {
-  let allPortfolios = allUsers[0].portfolio;
-
-  // Calculate the total assetBuyingPrice for each unique assetName
-  let totalAssetPriceMap = allPortfolios.reduce((acc, asset) => {
-    const { assetName, assetBuyingPrice } = asset;
-    acc[assetName] = (acc[assetName] || 0) + parseFloat(assetBuyingPrice);
-    return acc;
-  }, {});
-
-  // Convert the totalAssetPriceMap into an array of objects suitable for a composed chart
-  let composedChartData = Object.keys(totalAssetPriceMap).map((assetName) => ({
-    name: assetName,
-    uv: totalAssetPriceMap[assetName],
-  }));
+const PortfolioAssetChart = ({ totalCurrentPrice }) => {
+  const chartData = [
+    {
+      name: "Total Current Value",
+      uv: totalCurrentPrice,
+    },
+  ];
+ 
 
   return (
     <div className=" rounded-lg w-full overflow-x-auto">
-      <ComposedChart width={260} height={280} data={composedChartData}>
-        <CartesianGrid stroke="#313131" />
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        
-        <Area type="monotone" dataKey="uv" fill="#34ff71" stroke="#34ff71" />
-        <Bar dataKey="uv" barSize={20} fill="#2230ff" />
-        <Line type="monotone" dataKey="uv" stroke="#fff" />
-      </ComposedChart>
-    </div>
+    <ComposedChart width={260} height={250} data={chartData}>
+      <CartesianGrid stroke="#313131" />
+      <XAxis dataKey="name" />
+      <YAxis />
+      <Tooltip />
+      <Legend />
+      
+      <Area type="monotone" dataKey="uv" fill="#34ff71" stroke="#34ff71" />
+      <Bar dataKey="uv" barSize={20} fill="#2230ff" />
+      <Line type="monotone" dataKey="uv" stroke="#fff" />
+    </ComposedChart>
+  </div>
   );
 };
 
