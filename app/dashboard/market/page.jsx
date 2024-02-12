47fe7262d84a3feb6f6aaf89b946cc77d2a17855
srs-sieam <srssieam@gmail.com>
2024-02-12 09:54:57 +0600

    managed a disgusting bug

--- app/dashboard/market/page.jsx
+++ app/dashboard/market/page.jsx
@@ -52,6 +52,9 @@ const MarketPage = () => {
 
   const [category, setCategory] = useState('Cryptos');
   const [sort, setSort] = useState('Current Price');
+  const [assets, setAssets] = useState([]);
+  const [flatCurrency, setFlatCurrency] = useState([]);
+
 
   const { data: allCoins = [], isLoading, isError, refetch } = useQuery({
     queryKey: ['allCoins'],
@@ -61,9 +64,17 @@ const MarketPage = () => {
     }
   });
 
+  // console.log(allCoins)
+  useEffect(()=>{
+    if(allCoins.length > 0){
+      setAssets(allCoins.filter(coin => coin.type === "crypto coin"))
+      setFlatCurrency(allCoins.filter(coin => coin.type === "flat coin"))
+    }
+  },[allCoins])
+
 
   const createData = (name, key, price, type, changePrice, highPrice, lowPrice, icon) => ({ name, key, price, type, changePrice, highPrice, lowPrice, icon});
-  const [assets, setAssets] = useState(allCoins.filter(coin => coin.type === "crypto coin"));
+  
 
   // console.log(assets)
 
@@ -72,50 +83,55 @@ const MarketPage = () => {
 
     socket.addEventListener("message", (event) => {
       const data = JSON.parse(event.data);
-      const updatedAssets = assets.map((asset) => {
-        const ticker = data.find((item) => item.s === asset.key);
-        if (ticker) {
-          return createData(
-            asset.name,
-            asset.key,
-            parseFloat(ticker.c).toFixed(3),
-            asset.type,
-            parseFloat(ticker.p).toFixed(3),
-            parseFloat(ticker.h).toFixed(2),
-            parseFloat(ticker.l).toFixed(2),
-            asset.icon,
-          );
-        }
-        return asset;
-      });
-      setAssets(updatedAssets);
+      if(assets.length > 0){
+        const updatedAssets = assets.map((asset) => {
+          const ticker = data.find((item) => item.s === asset.key);
+          if (ticker) {
+            return createData(
+              asset.name,
+              asset.key,
+              parseFloat(ticker.c).toFixed(3),
+              asset.type,
+              parseFloat(ticker.p).toFixed(3),
+              parseFloat(ticker.h).toFixed(2),
+              parseFloat(ticker.l).toFixed(2),
+              asset.icon,
+            );
+          }
+          return asset;
+        });
+        setAssets(updatedAssets);
+      }
     });
     return () => socket.close();
   }, [assets]);
 
+
+
   const createFlatCurrencyData = (name, key, type, price, icon) => ({ name, key, type, price, icon });
-  const [flatCurrency, setFlatCurrency] = useState(allCoins.filter(coin => coin.type === "flat coin"));
 
   useEffect(() => {
     const fetchCurrencyRates = async () => {
       try {
-        const response = await axios.get(
-          'https://api.exchangerate-api.com/v4/latest/USD'
-        );
-        // Access the data property of the response to get the currency rates
-        const data = response.data.rates;
-        const updatedAssets = flatCurrency.map(cur => {
-          const currencyKey = cur.key
-          // console.log(currencyKey)
-          return createFlatCurrencyData(
-            cur.name,
-            cur.key,
-            cur.type,
-            data[currencyKey],
-            cur.icon,
+        if(flatCurrency.length > 0){
+          const response = await axios.get(
+            'https://api.exchangerate-api.com/v4/latest/USD'
           );
-        })
-        setFlatCurrency(updatedAssets)
+          // Access the data property of the response to get the currency rates
+          const data = response.data.rates;
+          const updatedAssets = flatCurrency.map(cur => {
+            const currencyKey = cur.key
+            // console.log(currencyKey)
+            return createFlatCurrencyData(
+              cur.name,
+              cur.key,
+              cur.type,
+              data[currencyKey],
+              cur.icon,
+            );
+          })
+          setFlatCurrency(updatedAssets)
+        }
       } catch (error) {
         console.error('Error fetching currency rates:', error);
       }
