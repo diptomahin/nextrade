1592a2be629d3be8c17700788ddd309b5377e665
mdnuruzzamannirob <nuruzzamanmd2002@gmail.com>
2024-02-15 00:34:48 +0600

    added the edt profile form with complete ui

--- components/traders_comp/profile/EditProfile.jsx
+++ components/traders_comp/profile/EditProfile.jsx
@@ -1,12 +1,18 @@
 import DarkButton from "@/components/library/buttons/DarkButton";
 import Image from "next/image";
 import { FaUserCircle } from "react-icons/fa";
+import { AiOutlineUser } from "react-icons/ai";
+import { TbUserStar } from "react-icons/tb";
+import { MdOutlineEmail } from "react-icons/md";
+import { IoMdPhonePortrait } from "react-icons/io";
+import { FaRegAddressBook } from "react-icons/fa6";
+import { PiCurrencyDollar, PiUpload } from "react-icons/pi";
 
 const EditProfile = ({ user }) => {
   return (
-    <form className="flex items-center justify-around gap-10 p-5">
+    <form className="flex items-center justify-around gap-10 px-5">
       {/* photo url */}
-      <div className="w-80 flex flex-col items-center justify-center">
+      <div className="flex-[2] flex flex-col items-center justify-center">
         {user?.photoURL !== undefined && user?.photoURL !== null ? (
           <Image
             alt="profile-image"
@@ -20,12 +26,18 @@ const EditProfile = ({ user }) => {
             <FaUserCircle />
           </p>
         )}
-        <div className="w-full flex flex-col mt-10">
-          <label htmlFor="" className="font-medium text-center">
-            Upload Photo
+        <div className="w-full flex flex-col mt-10 relative">
+          <label
+            htmlFor=""
+            className="w-full absolute top-0 flex flex-col items-center justify-center font-medium"
+          >
+            <PiUpload className="text-5xl" />{" "}
+            <span className="border-b-2 border-dashed border-secondary">
+              Upload Photo
+            </span>
           </label>
           <input
-            className="bg-transparent w-full border border-darkThree focus:border-darkGray text-xs mt-2 px-4 py-2 rounded-xl outline-none"
+            className="w-1/2 mx-auto h-20 opacity-0 z-10"
             type="file"
             id=""
             placeholder="file"
@@ -34,51 +46,20 @@ const EditProfile = ({ user }) => {
       </div>
 
       {/* user info */}
-      <div className="flex-1 flex flex-col gap-5 justify-center ">
-        {/* left side part */}
-        <div className="p-5">
-          <h3 className="text-xl font-semibold border-b w-fit">
-            Personal Information
-          </h3>
-
+      <div className="flex-[5] flex flex-col gap-10">
+        <h3 className="text-lg font-semibold border-b border-dashed border-secondary w-fit">
+          Update Information :
+        </h3>
+        <div className="">
           {/* first part */}
-          <div className="flex items-center gap-10 justify-between my-10">
+          <div className="flex items-center gap-5 justify-between">
             <div className="w-full flex flex-col">
-              <label htmlFor="" className="font-medium">
-                First Name
-              </label>
-              <input
-                className="bg-transparent w-full border border-darkThree focus:border-darkGray text-xs mt-2 px-4 py-2 rounded-xl outline-none"
-                type="text"
-                name="amount"
-                defaultValue={user?.displayName.split(" ").shift()}
-                id=""
-                placeholder="amount"
-              />
-            </div>
-            <div className="w-full flex flex-col">
-              <label htmlFor="" className="font-medium">
-                Last Name
-              </label>
-              <input
-                className="bg-transparent w-full border border-darkThree focus:border-darkGray text-xs mt-2 px-4 py-2 rounded-xl outline-none"
-                type="text"
-                name="amount"
-                defaultValue={user?.displayName.split(" ").slice(1)}
-                id=""
-                placeholder="amount"
-              />
-            </div>
-          </div>
-
-          {/* second part */}
-          <div className="flex items-center gap-10 justify-between">
-            <div className="w-full flex flex-col">
-              <label htmlFor="" className="font-medium">
+              <label htmlFor="" className="flex items-center gap-1 font-medium">
+                <AiOutlineUser className="text-lg" />
                 Full Name
               </label>
               <input
-                className="bg-transparent w-full border border-darkThree focus:border-darkGray text-xs mt-2 px-4 py-2 rounded-xl outline-none"
+                className="bg-transparent w-full border border-darkThree focus:border-darkGray text-sm mt-2 px-4 py-[10px] rounded-xl outline-none"
                 type="text"
                 name="amount"
                 defaultValue={user?.displayName}
@@ -87,53 +68,15 @@ const EditProfile = ({ user }) => {
               />
             </div>
             <div className="w-full flex flex-col">
-              <label htmlFor="" className="font-medium">
+              <label htmlFor="" className="flex items-center gap-1 font-medium">
+                <TbUserStar className="text-lg" />
                 User Name
               </label>
               <input
-                className="bg-transparent w-full border border-darkThree focus:border-darkGray text-xs mt-2 px-4 py-2 rounded-xl outline-none"
+                className="bg-transparent w-full border border-darkThree focus:border-darkGray text-sm mt-2 px-4 py-[10px] rounded-xl outline-none"
                 type="text"
                 name="amount"
                 defaultValue={user?.email}
-                id=""
-                placeholder="amount"
-              />
-            </div>
-          </div>
-        </div>
-
-        <hr className="border-darkThree" />
-
-        {/* right side part */}
-        <div className="p-5">
-          <h3 className="text-xl font-semibold border-b w-fit">
-            Contact Information
-          </h3>
-
-          {/* first part */}
-          <div className="flex items-center gap-10 justify-between my-10">
-            <div className="w-full flex flex-col">
-              <label htmlFor="" className="font-medium">
-                Email
-              </label>
-              <input
-                className="bg-transparent w-full border border-darkThree focus:border-darkGray text-xs mt-2 px-4 py-2 rounded-xl outline-none"
-                type="text"
-                name="amount"
-                defaultValue={user?.displayName.split(" ").shift()}
-                id=""
-                placeholder="amount"
-              />
-            </div>
-            <div className="w-full flex flex-col">
-              <label htmlFor="" className="font-medium">
-                Phone
-              </label>
-              <input
-                className="bg-transparent w-full border border-darkThree focus:border-darkGray text-xs mt-2 px-4 py-2 rounded-xl outline-none"
-                type="text"
-                name="amount"
-                defaultValue={user?.displayName.split(" ").slice(1)}
                 id=""
                 placeholder="amount"
               />
@@ -141,26 +84,14 @@ const EditProfile = ({ user }) => {
           </div>
 
           {/* second part */}
-          <div className="flex items-center gap-10 justify-between">
+          <div className="flex items-center gap-5 justify-between my-10">
             <div className="w-full flex flex-col">
-              <label htmlFor="" className="font-medium">
-                Address
+              <label htmlFor="" className="flex items-center gap-1 font-medium">
+                <MdOutlineEmail className="text-lg" />
+                Email
               </label>
               <input
-                className="bg-transparent w-full border border-darkThree focus:border-darkGray text-xs mt-2 px-4 py-2 rounded-xl outline-none"
-                type="text"
-                name="amount"
-                defaultValue={user?.displayName}
-                id=""
-                placeholder="amount"
-              />
-            </div>
-            <div className="w-full flex flex-col">
-              <label htmlFor="" className="font-medium">
-                Currency
-              </label>
-              <input
-                className="bg-transparent w-full border border-darkThree focus:border-darkGray text-xs mt-2 px-4 py-2 rounded-xl outline-none"
+                className="bg-transparent w-full border border-darkThree focus:border-darkGray text-sm mt-2 px-4 py-[10px] rounded-xl outline-none"
                 type="text"
                 name="amount"
                 defaultValue={user?.email}
@@ -168,8 +99,63 @@ const EditProfile = ({ user }) => {
                 placeholder="amount"
               />
             </div>
+            <div className="w-full flex flex-col">
+              <label htmlFor="" className="flex items-center gap-1 font-medium">
+                <IoMdPhonePortrait className="text-lg" />
+                Phone
+              </label>
+              <input
+                className="bg-transparent w-full border border-darkThree focus:border-darkGray text-sm mt-2 px-4 py-[10px] rounded-xl outline-none"
+                type="text"
+                name="amount"
+                defaultValue={"+8801973875893"}
+                id=""
+                placeholder="amount"
+              />
+            </div>
+          </div>
+
+          {/* third part */}
+          <div className="flex items-center gap-5 justify-between">
+            <div className="w-full flex flex-col">
+              <label htmlFor="" className="flex items-center gap-1 font-medium">
+                <FaRegAddressBook className="text-base" />
+                Address
+              </label>
+              <input
+                className="bg-transparent w-full border border-darkThree focus:border-darkGray text-sm mt-2 px-4 py-[10px] rounded-xl outline-none"
+                type="text"
+                name="amount"
+                defaultValue={"Jatrabari, Dhaka"}
+                id=""
+                placeholder="amount"
+              />
+            </div>
+            <div className="w-full flex flex-col">
+              <label htmlFor="" className="flex items-center gap-1 font-medium">
+                <PiCurrencyDollar className="text-lg" /> Currency
+              </label>
+              <select
+                name=""
+                id=""
+                className="bg-transparent w-full border border-darkThree focus:border-darkGray text-sm mt-2 px-4 py-[10px] rounded-xl outline-none"
+                defaultValue="usd"
+              >
+                <option value="" disabled>
+                  Select Currency
+                </option>
+                <option value="usd">USD</option>
+                <option value="bdt">BDT</option>
+                <option value="eur">EUR</option>
+                <option value="gbp">GBP</option>
+                <option value="inr">INR</option>
+                <option value="idr">IDR</option>
+                <option value="aed">AED</option>
+              </select>
+            </div>
           </div>
         </div>
+
         <div className="flex items-center justify-end gap-5">
           <DarkButton className="px-10">Cancel</DarkButton>
           <DarkButton className="px-10">Save</DarkButton>
--- components/traders_comp/profile/MyProfile.jsx
+++ components/traders_comp/profile/MyProfile.jsx
@@ -1,79 +1,99 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import { FaUserCircle } from "react-icons/fa";
 import useAuth from "@/hooks/useAuth";
+import { MdEditSquare } from "react-icons/md";
+import DarkButton from "@/components/library/buttons/DarkButton";
+import EditProfile from "./EditProfile";
 
 const MyProfile = () => {
+  const [isEdit, setIsEdit] = useState(false);
   const { user } = useAuth();
 
   return (
-    <div className="w-4/6 mx-auto bg-gradient-to-br from-darkOne to-darkTwo border border-darkThree flex flex-col gap-10 rounded-xl p-5">
-      <div className="w-full flex items-center justify-between">
-        <h2>My Profile</h2>
-        <button>Edit</button>
-      </div>
-
-      <div className="w-full border border-dashed border-darkThree"></div>
-
-      {/* user info */}
-      <div className="flex flex-col items-center gap-5 ">
-        {/* photo url */}
-        <div className="flex items-center justify-center">
-          {user?.photoURL !== undefined && user?.photoURL !== null ? (
-            <Image
-              alt="profile-image"
-              width={150}
-              height={150}
-              src={user?.photoURL}
-              className="rounded-full"
-            />
-          ) : (
-            <p className="text-5xl text-primary">
-              <FaUserCircle />
-            </p>
-          )}
+    <div
+      style={{ minHeight: "calc(100vh - 107px)" }}
+      className="flex items-center justify-center py-5"
+    >
+      <div className="5xl:w-4/6 bg-gradient-to-br from-darkOne to-darkTwo border border-darkThree flex flex-col gap-10 rounded-xl px-5 pb-8">
+        <div className="w-full flex items-center justify-between border-b border-dashed border-darkThree p-5">
+          <h2 className="text-xl font-semibold">My Profile</h2>
+          <DarkButton
+            onClick={() => setIsEdit(!isEdit)}
+            className="px-5 rounded-md xl:rounded-md"
+          >
+            <MdEditSquare /> Edit
+          </DarkButton>
         </div>
 
-        <div className="w-full flex gap-5">
-          {/* left side part */}
-          <div className="flex-1 space-y-8 p-5">
-            <div className="space-y-2">
-              <p className="text-sm font-medium opacity-70">Full Name</p>
-              <p className="font-semibold">{user?.displayName}</p>
+        {/* user info */}
+        {isEdit ? (
+          <EditProfile user={user} />
+        ) : (
+          <div className="flex flex-col items-center gap-5 ">
+            {/* photo url */}
+            <div className="flex items-center justify-center">
+              {user?.photoURL !== undefined && user?.photoURL !== null ? (
+                <Image
+                  alt="profile-image"
+                  width={150}
+                  height={150}
+                  src={user?.photoURL}
+                  className="rounded-full"
+                />
+              ) : (
+                <p className="text-5xl text-primary">
+                  <FaUserCircle />
+                </p>
+              )}
             </div>
-            <div className="space-y-2">
-              <p className="text-sm font-medium opacity-70">Email Address</p>
-              <p className="font-semibold">{user?.email}</p>
-            </div>
-            <div className="space-y-2">
-              <p className="text-sm font-medium opacity-70">Address</p>
-              <p className="font-semibold">Jatrabari, Dhaka</p>
-            </div>
-          </div>
 
-          {/* right side part */}
-          <div className="flex-1 space-y-8 p-5">
-            <div className="space-y-2">
-              <p className="text-sm font-medium opacity-70">User Name</p>
-              <p className="font-semibold">{user?.email}</p>
+            <div className="w-full flex gap-5">
+              {/* left side part */}
+              <div className="flex-1 space-y-8 p-5">
+                <div className="space-y-2">
+                  <p className="text-sm font-medium opacity-70">Full Name</p>
+                  <p className="font-semibold">{user?.displayName}</p>
+                </div>
+                <div className="space-y-2">
+                  <p className="text-sm font-medium opacity-70">
+                    Email Address
+                  </p>
+                  <p className="font-semibold">{user?.email}</p>
+                </div>
+                <div className="space-y-2">
+                  <p className="text-sm font-medium opacity-70">Address</p>
+                  <p className="font-semibold">Jatrabari, Dhaka</p>
+                </div>
+              </div>
+
+              {/* right side part */}
+              <div className="flex-1 space-y-8 p-5">
+                <div className="space-y-2">
+                  <p className="text-sm font-medium opacity-70">User Name</p>
+                  <p className="font-semibold">{user?.email}</p>
+                </div>
+                <div className="space-y-2">
+                  <p className="text-sm font-medium opacity-70">Phone Number</p>
+                  <p className="font-semibold">+8801973875893</p>
+                </div>
+                <div className="space-y-2">
+                  <p className="text-sm font-medium opacity-70">Currency</p>
+                  <p className="font-semibold">$USD</p>
+                </div>
+              </div>
             </div>
-            <div className="space-y-2">
-              <p className="text-sm font-medium opacity-70">Phone Number</p>
-              <p className="font-semibold">+8801973875893</p>
-            </div>
-            <div className="space-y-2">
-              <p className="text-sm font-medium opacity-70">Currency</p>
-              <p className="font-semibold">$USD</p>
+            <div className="w-full flex items-center justify-between gap-5 px-5">
+              <p className="text-xs">
+                This account was created on January 10, 2024
+              </p>
+              <button className="btn btn-sm px-5 h-8 bg-red-600 hover:bg-red-600 border-none text-white text-xs">
+                Delete Account
+              </button>
             </div>
           </div>
-        </div>
-      </div>
-      <div className="w-full flex items-center justify-between gap-5 px-5">
-        <p className="text-sm">This account was created on January 10, 2024</p>
-        <button className="btn btn-sm px-5 h-8 bg-red-600 hover:bg-red-600 border-none text-white text-xs">
-          Delete Account
-        </button>
+        )}
       </div>
     </div>
   );
