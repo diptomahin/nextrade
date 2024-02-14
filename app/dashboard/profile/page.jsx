e541a3bf80ef41c196d65a603e868e49aa2b4e8f
mdtajulislammt <mdtajulislammt505@gmail.com>
2024-02-14 17:33:16 +0600

    dashboard/designProfile

--- app/dashboard/profile/page.jsx
+++ app/dashboard/profile/page.jsx
@@ -9,6 +9,7 @@ import { FaUserCircle } from "react-icons/fa";
 import { CiLock, CiUser } from "react-icons/ci";
 import { PiCurrencyDollarThin, PiCardholderThin } from "react-icons/pi";
 import { MdArrowBackIosNew } from "react-icons/md";
+import MyProfile from "@/components/traders_comp/profile/MyProfile";
 
 const ProfilePage = () => {
   const [isActiveProfile, setIsActiveProfile] = useState(false);
@@ -176,104 +177,9 @@ const ProfilePage = () => {
           isActiveProfile ? "xl:pl-[98px]" : "xl:pl-[260px]"
         }`}
       >
+        
         <TabPanel>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem sint
-          recusandae quos distinctio unde expedita sapiente labore incidunt quas
-          eligendi excepturi laboriosam impedit, aliquam, deserunt dolorum
-          adipisci cum corporis autem dolore dignissimos quod, soluta ea! Quos
-          explicabo. Nobis minus enim odit officiis quo! Asperiores ab
-          architecto, debitis esse rem fugit maiores nesciunt consequatur eum
-          accusantium pariatur repellat sit inventore quidem molestias, libero,
-          assumenda perferendis numquam. Quaerat ad aperiam corporis, harum
-          ipsum dolores eius pariatur natus amet commodi est expedita odit
-          consequuntur ipsam! Recusandae quaerat dolor porro quo ducimus omnis
-          ab quod reprehenderit perspiciatis molestiae, temporibus, expedita
-          delectus quidem qui unde voluptates excepturi sint velit vero eveniet
-          asperiores? Eaque provident asperiores perferendis corporis nulla,
-          incidunt quia ratione nisi voluptatibus autem cupiditate libero illo a
-          voluptas vel modi recusandae veritatis error quidem. Consectetur
-          quisquam ipsa veritatis, voluptates tenetur aliquam. Facere mollitia,
-          exercitationem nisi hic numquam repudiandae voluptatem ipsum similique
-          commodi a alias necessitatibus quia itaque expedita quo rem magnam
-          sint doloremque provident voluptate natus odio. Voluptate unde saepe
-          illo est suscipit facilis, distinctio neque beatae aspernatur
-          voluptatum ut dolorum temporibus repudiandae iure consequatur quasi,
-          exercitationem nulla quis tempore similique! Quae ipsa placeat odit
-          laborum, officiis quo rem accusamus atque expedita pariatur iste
-          eligendi, minus aliquid deleniti porro. Repellat voluptatibus suscipit
-          a esse illo ullam, quis ut, dolorem omnis atque aut! Similique maxime
-          obcaecati, provident saepe, earum aliquid repudiandae voluptatum
-          dolorum numquam, doloremque eveniet? Dignissimos sequi possimus error
-          culpa eveniet placeat mollitia saepe fuga, libero consequatur vel
-          aspernatur recusandae nulla odit sit assumenda consequuntur
-          accusantium delectus necessitatibus reiciendis laudantium eum quos
-          doloribus repellendus. Impedit voluptatem rem incidunt voluptate
-          commodi aperiam dolorem, sapiente repellendus, ut dignissimos
-          laudantium blanditiis modi eaque quam pariatur enim corporis. Dolore,
-          vel. Odio, consequatur doloremque. Accusantium ipsum perferendis
-          deserunt? Quam facilis ratione maxime rem numquam fugiat vero enim ex.
-          A quaerat non, rerum obcaecati cum similique dolorum, dolore quidem
-          tempore, officia debitis ex maxime harum ratione repellat magnam
-          aspoluptatibus laborum sit voluptate soluta animi quia aliquam impedit
-          magnam, reprehenderit adipisci blanditiis maiores illum aperiam sequi
-          maxime. Natus, magni cumque aut praesentium nostrum, libero id
-          corrupti ipsum nulla temporibus nihil, dolor blanditiis explicabo
-          deserunt repudiandae maxime quos vitae exercitationem eum animi est
-          voluptatibus dignissimos. Quod sequi suscipit quam, assumenda dicta
-          facilis quae exercitationem, minima architecto cupiditate repudiandae
-          autem aliquam, incidunt praesentium nostrum eligendi laudantium
-          consectetur hic veniam eaque corrupti nesciunt necessitatibus atque
-          qui. Quos unde sunt ducimus error autem saepe, inventore suscipit enim
-          illum vitae ipsa perspiciatis exercitationem iste harum? Quisquam
-          nihil sequi dolor voluptas accusantium neque molestias ducimus
-          obcaecati, ut minima ex, soluta, laboriosam blanditiis explicabo
-          placeat. Odit eum perferendis aperiam ullam similique delectus minima
-          assumenda cumque molestiae ipsa recusandae nemo corrupti rem possimus
-          temporibus, ea adipisci, natus ut atque? Ad enim reiciendis sed
-          doloremque architecto iusto ducimus? At, dolorem cum ratione nulla sed
-          error a pariatur vero officiis nostrum natus, explicabo veniam illum?
-          Porro deleniti vero facilis in magni reprehenderit assumenda,
-          doloremque velit aspernatur debitis aliquam cupiditate et maxime ab
-          cum blanditiis corrupti consectetur! Nostrum repellat modi quis
-          laboriosam, unde accusamus numquam optio iusto ipsa autem debitis
-          reiciendis cumque ex necessitatibus sunt ab dolorem tempore recusandae
-          consequuntur odio dolores eligendi! Recusandae commodi saepe facilis
-          beatae ipsa quia quo perspiciatis, nisi excepturi sed autem impedit.
-          Temporibus beatae ab consequatur fuga nulla officia deleniti fugit
-          alias cum facilis, porrchitecto voluptate ratione impedit natus
-          commodi eveniet similique saepe repellat excepturi voluptates qui
-          maiores nesciunt tempora. Vero incidunt fugit veniam enim a, commodi
-          exercitationem hic dolore ipsa veritatis nostrum officia aliquam ea
-          non libero eius adipisci neque consectetur voluptas. Nulla corrupti
-          voluptatem beatae voluptas harum repellendus repudiandae recusandae
-          maxime. Doloremque accusamus dolorum suscipit sint quia maiores. Nobis
-          repellendus nostrum eius deleniti explicabo tempore soluta? Tempore
-          laboriosam quia perferendis iusto expedita nulla ad, quibusdam ipsa
-          aliquid totam ipsam odit atque, mollitia soluta! Rerum laborum modi
-          facilis expedita aperiam itaque obcaecati consectetur ea, nesciunt
-          aliquid cum repellat maiores repudiandae enim beatae! Sunt voluptas
-          hic necessitatibus commodi repudiandae blanditiis delectus suscipit
-          enim veniam? Illum qui cum libero sed minus harum minima, esse quam
-          pariatur? Itaque harum error quam voluptatum excepturi iste at. Iusto
-          esse pariatur necessitatibus dignissimos inventore laborum, provident
-          ullam adipisci illo qui non repellendus doloribus odio quaerat
-          sapiente quia beatae nam itaque voluptas. Aliquid delectus debitis
-          quae maiores officia illo. Suscipit ullam dignissimos dolore doloribus
-          quibusdam cum fuga ratione ducimus natus, nostrum velit accusantium ab
-          necessitatibus minus praesentium quod doloremque magnam ex libero.is
-          iusto expedita nulla ad, quibusdam ipsa aliquid totam ipsam odit
-          atque, mollitia soluta! Rerum laborum modi facilis expedita aperiam
-          itaque obcaecati consectetur ea, nesciunt aliquid cum repellat maiores
-          repudiandae enim beatae! Sunt voluptas hic necessitatibus commodi
-          repudiandae blanditiis delectus suscipit enim veniam? Illum qui cum
-          libero sed minus harum minima, esse quam pariatur? Itaque harum error
-          quam voluptatum excepturi iste at. Iusto esse pariatur necessitatibus
-          dignissimos inventore laborum, provident ullam adipisci illo qui non
-          repellendus doloribus odio quaerat sapiente quia beatae nam itaque
-          voluptas. Aliquid delectus debitis quae maiores officia illo. Suscipit
-          ullam dignissimos dolore doloribus quibusdam cum fuga ratione ducimus
-          natus, nostrum velit accusantium ab necessitatibus minus praesentium
-          quod do Optio.
+          <MyProfile></MyProfile>
         </TabPanel>
         <TabPanel>2</TabPanel>
         <TabPanel>3</TabPanel>
