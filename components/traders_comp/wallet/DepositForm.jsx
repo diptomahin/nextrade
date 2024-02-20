04b7749b866af9297e19cb633e23cb706b65b81d
mdnuruzzamannirob <nuruzzamanmd2002@gmail.com>
2024-02-20 23:34:17 +0600

    fix some bugs on dashboard wallet page

--- components/traders_comp/wallet/DepositForm.jsx
+++ components/traders_comp/wallet/DepositForm.jsx
@@ -14,7 +14,7 @@ import useAuth from "@/hooks/useAuth";
 const DepositForm = ({ refetch, date, userBalanceRefetch }) => {
   const [paymentError, setPaymentError] = React.useState("");
   const [clientSecret, setClientSecret] = React.useState("");
-  const [amount, setAmount] = React.useState("");
+  const [amount, setAmount] = React.useState(0);
   const [postalCode, setPostalCode] = React.useState(0);
   // const [currency, setCurrency] = React.useState("");
   const stripe = useStripe();
@@ -22,8 +22,11 @@ const DepositForm = ({ refetch, date, userBalanceRefetch }) => {
   const { user } = useAuth();
 
   React.useEffect(() => {
+    if (amount <= 0 || !amount) {
+      return;
+    }
     if (amount > 100000) {
-      return setPaymentError("The amount must be 100,000 or less.");
+      return setPaymentError("*The amount must be 100,000 or less.");
     }
 
     axios
@@ -43,12 +46,20 @@ const DepositForm = ({ refetch, date, userBalanceRefetch }) => {
 
     if (!/^-?\d*\.?\d+$/.test(amount)) {
       form.reset();
-      setAmount("");
+      setAmount(0);
       return setPaymentError("*Please provide a valid number amount");
     }
 
+    if (amount <= 0 || !amount) {
+      return setPaymentError("*Please provide a valid amount");
+    }
+
+    if (amount > 100000) {
+      return setPaymentError("*The amount must be 100,000 or less.");
+    }
+
     if (!/^\d{4}$/.test(postalCode)) {
-      setPostalCode("");
+      setPostalCode(0);
       return setPaymentError("*Please provide a valid 4-digit postal code");
     }
 
@@ -113,8 +124,8 @@ const DepositForm = ({ refetch, date, userBalanceRefetch }) => {
           .then((res) => {
             if (res.data.insertedId) {
               form.reset();
-              setAmount("");
-              setPostalCode("");
+              setAmount(0);
+              setPostalCode(0);
               elements.getElement(CardNumberElement).clear(); // Reset card number
               elements.getElement(CardExpiryElement).clear(); // Reset card expiry
               elements.getElement(CardCvcElement).clear();
