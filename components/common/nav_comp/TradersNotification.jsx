feef8f118a2049e67ec8c103ee68b69fa5d1e0d9
mdtajulislammt <mdtajulislammt505@gmail.com>
2024-02-21 04:06:04 +0600

    dashboard/notifications

--- components/common/nav_comp/TradersNotification.jsx
+++ components/common/nav_comp/TradersNotification.jsx
@@ -1,13 +1,31 @@
 import Image from "next/image";
-import React from "react";
+import React, { useEffect } from "react";
 import { MdNotifications, MdNotificationsActive } from "react-icons/md";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import SettingsIcon from "@mui/icons-material/Settings";
 import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import useAuth from "@/hooks/useAuth";
+import useSecureFetch from "@/hooks/useSecureFetch";
 
 const TradersNotification = () => {
   const [isNotificationOpen, setIsNotificationOpen] = React.useState(false);
   const [isOpenDot, setIsOpenDot] = React.useState(false);
+  const [notiData,setNotiData] = React.useState('');
+  const {user} = useAuth()
+
+  const {
+    data: notificationsData = [],
+    refetch,
+    isPending,
+    isLoading,
+  } = useSecureFetch(`/notifications?email=${user.email}`, ["notifications"]);
+
+  React.useEffect(()=>{
+    notificationsData.map(item=>setNotiData(item))
+    refetch()
+  },[notificationsData,refetch])
+  console.log(notiData);
+
   return (
     <div className="relative">
       <button
@@ -21,6 +39,7 @@ const TradersNotification = () => {
         ) : (
           <MdNotifications className="w-6 h-6" />
         )}
+        <p className="absolute left-3 -top-2 font-semibold w-5 h-5 p-[2px] text-sm  rounded-full bg-red-500 flex justify-center items-center">{ notificationsData?.length }</p>
       </button>
       {isNotificationOpen && (
         <div className="absolute top-12 md:right-40 right-24 transform translate-x-1/2  rounded bg-gradient-to-bl from-darkOne to-darkTwo border border-darkThree flex flex-col gap-2 p-3 md:w-96 w-80  ">
@@ -34,13 +53,15 @@ const TradersNotification = () => {
 
           <div className="bg-darkThree p-[0.8px] my-2"></div>
           {/*  */}
-          <div className=" flex justify-between gap-2 bg-darkBG border border-darkThree p-4 rounded-xl">
+          {notificationsData.map(asset=>
+          // cart section
+          <div key={asset._id} className=" flex justify-between gap-2 bg-darkBG border border-darkThree p-4 rounded-xl">
             <div className=" flex items-center gap-3">
               <NotificationsNoneIcon/>
               <div>
-                <h2 className=" font-medium">Lorem ipsum dolor sit amet.</h2>
+                <h2 className=" font-medium">{asset.title}</h2>
                 <p className=" text-gray-400 text-sm">
-                  Lorem ipsum dolor sit
+                  {asset.description}
                 </p>
               </div>
             </div>
@@ -60,7 +81,7 @@ const TradersNotification = () => {
                 </div>
               )}
             </div>
-          </div>
+          </div>)}
         </div>
       )}
     </div>
