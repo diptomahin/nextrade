398358e7f9c12388a6477c993cf41f22fdd396af
mdnuruzzamannirob <nuruzzamanmd2002@gmail.com>
2024-02-21 11:46:35 +0600

    why nextrade and homepage converted

--- app/page.jsx
+++ app/page.jsx
@@ -8,25 +8,8 @@ import Subscription from "@/components/home_comp/subscription/Subscription";
 import TradingStep from "@/components/home_comp/TradingStep";
 import WhyNexTrade from "@/components/home_comp/WhyNexTrade";
 import Modal from "@/components/home_comp/Modal";
-import useAuth from "@/hooks/useAuth";
-// import { FacebookProvider, CustomChat } from "react-facebook";
 
 export default function Home() {
-  const { loading } = useAuth();
-
-  if (loading) {
-    return (
-      <div className="h-screen w-full flex justify-center items-center">
-        <div className="text-5xl text-primary font-semibold">
-          Loading
-          <span className="text-secondary">
-            .<span className="text-primary">.</span>.
-          </span>
-        </div>
-      </div>
-    );
-  }
-
   return (
     <main className="overflow-hidden text-zinc-100 font-montserrat">
       <Modal />
