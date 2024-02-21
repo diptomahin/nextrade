a680d270ea41400f5f93b42233c5affea1dfc0b1
jasaiful <jasaiful@gmail.com>
2024-02-22 00:07:58 +0600

    created initial testimonial page

---
+++ components/home_comp/Testimonials.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Testimonials = () => {
+    return (
+        <div>
+            <h2>Testimonials Start from here</h2>
+        </div>
+    );
+};
+
+export default Testimonials;
