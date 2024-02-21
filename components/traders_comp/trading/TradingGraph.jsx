770f32003d546d931542bd6bdc99c67aba8b7a4d
mahindipto <mahindipto18@gmail.com>
2024-02-21 16:46:21 +0600

    cutomized dropdown design

--- components/traders_comp/trading/TradingGraph.jsx
+++ components/traders_comp/trading/TradingGraph.jsx
@@ -36,31 +36,9 @@ const TradingGraph = (params) => {
         />
         ) :
         (
-          <AdvancedRealTimeChart
-          width="100%"
-          height="100%"
-          autosize
-          symbol={`BTCUSD`}
-          interval={20}
-          range="1M"
-          timezone="UTC"
-          theme="dark"
-          style={1}
-          locale="en"
-          toolbar_bg="#f1f3f6"
-          enable_publishing={false}
-          hide_top_toolbar={true}
-          hide_legend={true}
-          withdateranges={false}
-          hide_side_toolbar={true}
-          details={false}
-          hotlist={false}
-          calendar={false}
-          studies={[]}
-          disabled_features={[]}
-          enabled_features={[]}
-          container_id="advanced-chart-widget-container"
-        />
+         <h3 className="text-4xl text-center my-auto p-10 border-primary border-x-4 border-y-4 font-bold text-white">
+          Select An Asset <br/> From The Dropdown
+         </h3>
         )
       }
       </div>
