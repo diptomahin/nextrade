b2d9b099f5c49b58cab17ed9c6f1084d952a46e0
Ariful Islam <hello.mrariful@gmail.com>
2024-02-21 15:50:31 +0600

    add the google translate widget

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,4 @@
 "use client";
-import Translate from "@/components/home_comp/Translate";
 import Banner from "@/components/home_comp/banner/Banner";
 import InvestSolutions from "@/components/home_comp/InvestSolutions";
 import Map from "@/components/home_comp/map/Map";
@@ -10,13 +9,14 @@ import TradingStep from "@/components/home_comp/TradingStep";
 import WhyNexTrade from "@/components/home_comp/WhyNexTrade";
 import Modal from "@/components/home_comp/Modal";
 import CustomerChat from "@/components/home_comp/CustomerChat";
+import Translate from "@/components/home_comp/Translate";
 
 export default function Home() {
   return (
     <main className="overflow-hidden text-zinc-100 font-montserrat">
-      <Translate />
       <Modal />
       <Banner />
+      <Translate />
       <TradingStep />
       <InvestSolutions />
       <StartTrading />
