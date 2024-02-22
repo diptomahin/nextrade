bce7abaf602b6d24faac8c1513861e23641e0d3b
jasaiful <jasaiful@gmail.com>
2024-02-22 21:14:52 +0600

    created initial feedback component

---
+++ components/traders_comp/feedback/FeedbackForm.jsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from 'react';
+
+
+const FeedbackForm = () => {
+    return (
+        <div>
+            <h1>Feedback Form</h1>
+        </div>
+    );
+};
+
+export default FeedbackForm;
