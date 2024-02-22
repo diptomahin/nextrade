f2063ba7db9ce2547870e889c696f8ef9b5bc516
Ariful Islam <hello.mrariful@gmail.com>
2024-02-23 03:42:26 +0600

    make sectionTitle client to server component and remove all unnecessary comments

--- app/dashboard/academy/SectionTitle.jsx
+++ app/dashboard/academy/SectionTitle.jsx
@@ -1,36 +1,33 @@
-"use client"
 import Link from "next/link";
 
 const SectionTitle = ({ title, btnText, btnUrl }) => {
-
   return (
     <div className="flex justify-between items-center">
       <h2 className="lg:text-[30px] text-xl font-semibold">{title}</h2>
       <Link href={btnUrl ?? "#"}>
-      <button
-  className="relative font-semibold uppercase lg:text-normal text-[14px]"
-  style={{
-    display: 'inline-block',
-    paddingBottom: '2px',
-    backgroundImage: 'linear-gradient(white, white)', // Change to white gradient
-    backgroundPosition: '0 100%', // OR 'bottom left'
-    backgroundSize: '0% 2px',
-    backgroundRepeat: 'no-repeat',
-    transition: 'background-size 0.3s, background-position 0s 0.3s', // change after the size immediately
-  }}
-  onMouseEnter={(e) => {
-    e.target.style.backgroundPosition = '100% 100%'; // OR 'bottom right'
-    e.target.style.backgroundSize = '100% 2px';
-  }}
-  onMouseLeave={(e) => {
-    e.target.style.backgroundPosition = '0 100%'; // OR 'bottom left'
-    e.target.style.backgroundSize = '0% 2px';
-  }}
->
-  <span className="absolute bottom-0 left-0 bg-white" />
-  {btnText}
-</button>
-
+        <button
+          className="relative font-semibold uppercase lg:text-normal text-[14px]"
+          style={{
+            display: "inline-block",
+            paddingBottom: "2px",
+            backgroundImage: "linear-gradient(white, white)",
+            backgroundPosition: "0 100%",
+            backgroundSize: "0% 2px",
+            backgroundRepeat: "no-repeat",
+            transition: "background-size 0.3s, background-position 0s 0.3s",
+          }}
+          onMouseEnter={(e) => {
+            e.target.style.backgroundPosition = "100% 100%";
+            e.target.style.backgroundSize = "100% 2px";
+          }}
+          onMouseLeave={(e) => {
+            e.target.style.backgroundPosition = "0 100%";
+            e.target.style.backgroundSize = "0% 2px";
+          }}
+        >
+          <span className="absolute bottom-0 left-0 bg-white" />
+          {btnText}
+        </button>
       </Link>
     </div>
   );
