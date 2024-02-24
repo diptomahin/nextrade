50febdfa0e2bc1e8a338ed590a0075b72a59aa3f
Ariful Islam <hello.mrariful@gmail.com>
2024-02-24 23:46:34 +0600

    remove translator to desfined the error

--- components/home_comp/Translate.jsx
+++ components/home_comp/Translate.jsx
@@ -1,38 +1,31 @@
-"use client";
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import "./Translate.css";
 
-const Translate = () => {
-  const googleTranslateElementInit = () => {
-    new window.google.translate.TranslateElement(
-      {
-        pageLanguage: "en",
-        autoDisplay: false,
-      },
-      "google_translate_element"
-    );
-  };
-
-  useEffect(() => {
-    const addScript = document.createElement("script");
-    addScript.setAttribute(
-      "src",
-      "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
-    );
-    document.body.appendChild(addScript);
-    window.googleTranslateElementInit = googleTranslateElementInit;
-  
-    // Cleanup function to remove the Google Translate widget
-   // Cleanup function to remove the Google Translate widget
-return () => {
-  const translateElement = document.getElementById("google_translate_element");
-  if (translateElement && translateElement.parentNode) {
-    translateElement.parentNode.removeChild(translateElement);
-  }
+// Define googleTranslateElementInit globally
+window.googleTranslateElementInit = () => {
+  new window.google.translate.TranslateElement(
+    {
+      pageLanguage: "en",
+      autoDisplay: false
+    },
+    "google_translate_element"
+  );
 };
 
+const Translate = () => {
+  useEffect(() => {
+    // Load Google Translate script
+    const addScript = document.createElement("script");
+    addScript.src = "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
+    addScript.async = true;
+    document.body.appendChild(addScript);
+
+    // Cleanup function
+    return () => {
+      document.body.removeChild(addScript);
+      delete window.googleTranslateElementInit;
+    };
   }, []);
-  
 
   return <div id="google_translate_element"></div>;
 };
