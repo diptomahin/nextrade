4b5e51fc4b0bbe48c466d5cffbc4bbb15ef562dd
Ariful Islam <hello.mrariful@gmail.com>
2024-02-24 23:18:29 +0600

    trying to solve of the translate page

--- components/home_comp/Translate.jsx
+++ components/home_comp/Translate.jsx
@@ -21,7 +21,18 @@ const Translate = () => {
     );
     document.body.appendChild(addScript);
     window.googleTranslateElementInit = googleTranslateElementInit;
+  
+    // Cleanup function to remove the Google Translate widget
+   // Cleanup function to remove the Google Translate widget
+return () => {
+  const translateElement = document.getElementById("google_translate_element");
+  if (translateElement && translateElement.parentNode) {
+    translateElement.parentNode.removeChild(translateElement);
+  }
+};
+
   }, []);
+  
 
   return <div id="google_translate_element"></div>;
 };
