6e63525b91991c9ced32c602fa21a3fa22177460
srs-sieam <srssieam@gmail.com>
2024-02-25 18:10:23 +0600

    converted market page into server component

---
+++ hooks/useWatchlistData.jsx
@@ -0,0 +1,30 @@
+import useAuth from "./useAuth";
+import { useQuery } from "@tanstack/react-query";
+import useSecureAPI from "./useSecureAPI";
+
+const useWatchlistData = () => {
+  const useSecure = useSecureAPI();
+  const { user, loading } = useAuth();
+
+  //
+  const { data, isPending, isLoading, isError, refetch } = useQuery({
+    queryKey: [user?.email, "watchlist"],
+    queryFn: async () => {
+      if (loading) {
+        return;
+      }
+      const res = await useSecure.get(`/watchlist?email=${user.email}`);
+      return res.data;
+    },
+  });
+
+  return {
+    watchlistData: data || [],
+    refetchWatchlistData: refetch,
+    watchlistDataLoading: isLoading,
+    watchlistDataPending: isPending,
+    watchlistDataError: isError,
+  };
+};
+
+export default useWatchlistData;
