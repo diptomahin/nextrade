c4ac905d5170d1d4ce3870b8df4e185c6815f9f5
Ariful Islam <hello.mrariful@gmail.com>
2024-02-27 19:05:57 +0600

    just remove all unsolved code

--- components/home_comp/Translate.jsx
+++ components/home_comp/Translate.jsx
@@ -1,39 +1,27 @@
-import Script from "next/script";
-import React from "react";
+"use client"
+import { useEffect } from "react";
 
 const Translate = () => {
   const googleTranslateElementInit = () => {
     new window.google.translate.TranslateElement(
       {
         pageLanguage: "en",
-        autoDisplay: false,
+        autoDisplay: false
       },
       "google_translate_element"
     );
   };
-
-  React.useEffect(() => {
-    if (typeof window !== "undefined" && !window.google) {
-      const script = document.createElement("script");
-      script.src = "//translate.google.com/translate_a/element.js";
-      script.async = true;
-      script.onload = googleTranslateElementInit;
-      document.body.appendChild(script);
-    }
+  useEffect(() => {
+    let addScript = document.createElement("script");
+    addScript.setAttribute(
+      "src",
+      "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
+    );
+    document.body.appendChild(addScript);
+    window.googleTranslateElementInit = googleTranslateElementInit;
   }, []);
 
-  return (
-    <>
-      <Script id="google-translate-init" strategy="lazyOnload">
-        {`
-          window.googleTranslateElementInit = function() {
-            new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
-          };
-        `}
-      </Script>
-      <div id="google_translate_element" />
-    </>
-  );
+  return <div id="google_translate_element" />;
 };
 
 export default Translate;
