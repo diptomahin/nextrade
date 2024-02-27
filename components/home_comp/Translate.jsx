74c8821d42352d5633c254da1aa606fd23dcee2d
Ariful Islam <hello.mrariful@gmail.com>
2024-02-27 13:27:28 +0600

    add another page to render  in server side

--- components/home_comp/Translate.jsx
+++ components/home_comp/Translate.jsx
@@ -1,37 +1,15 @@
-import React from 'react';
-import "./Translate.css"
-import { useEffect } from "react";
+import dynamic from 'next/dynamic';
 
-const Translate = () => {
+const GoogleTranslate = dynamic(() => import('./GoogleTranslate'), {
+  ssr: false,
+});
 
-    const googleTranslateElementInit = () => {
-        new window.google.translate.TranslateElement(
-          {
-            pageLanguage: "en",
-            autoDisplay: false
-          },
-          "google_translate_element"
-        );
-      };
-      useEffect(() => {
-        const addScript = document.createElement("script");
-        addScript.setAttribute(
-          "src",
-          "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
-        );
-        document.body.appendChild(addScript);
-        window.googleTranslateElementInit = googleTranslateElementInit;
-      }, []);
-
-    return (
-
-        <div>
-          <h1 id="google_translate_element">
-
-          </h1>
-        </div>
-
-    );
+const Home = () => {
+  return (
+    <div>
+      <GoogleTranslate />
+    </div>
+  );
 };
 
-export default Translate;
+export default Home;
