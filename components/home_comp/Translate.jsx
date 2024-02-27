b4f6087e5d051c7908cda2be1cb95443118fe4d6
Ariful Islam <hello.mrariful@gmail.com>
2024-02-27 18:51:40 +0600

    use next script to solve the unhandel error

--- components/home_comp/Translate.jsx
+++ components/home_comp/Translate.jsx
@@ -1,15 +1,39 @@
-import dynamic from 'next/dynamic';
+import Script from "next/script";
+import React from "react";
 
-const GoogleTranslate = dynamic(() => import('./GoogleTranslate'), {
-  ssr: false,
-});
+const Translate = () => {
+  const googleTranslateElementInit = () => {
+    new window.google.translate.TranslateElement(
+      {
+        pageLanguage: "en",
+        autoDisplay: false,
+      },
+      "google_translate_element"
+    );
+  };
 
-const Home = () => {
+  React.useEffect(() => {
+    if (typeof window !== "undefined" && !window.google) {
+      const script = document.createElement("script");
+      script.src = "//translate.google.com/translate_a/element.js";
+      script.async = true;
+      script.onload = googleTranslateElementInit;
+      document.body.appendChild(script);
+    }
+  }, []);
+
   return (
-    <div>
-      <GoogleTranslate />
-    </div>
+    <>
+      <Script id="google-translate-init" strategy="lazyOnload">
+        {`
+          window.googleTranslateElementInit = function() {
+            new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+          };
+        `}
+      </Script>
+      <div id="google_translate_element" />
+    </>
   );
 };
 
-export default Home;
+export default Translate;
