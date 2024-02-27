0fc5f110fd0c4736cd388c29e5ce7d42d01b0ff0
srs-sieam <srssieam@gmail.com>
2024-02-27 07:22:57 +0600

    converted manage user page into server component

--- next.config.js
+++ next.config.js
@@ -3,6 +3,6 @@ const nextConfig = {}
 
 module.exports = {
   images: {
-    domains: ['assets.coingecko.com', 'lh3.googleusercontent.com', 'i.ibb.co', 'new.axilthemes.com', 'img.freepik.com'],
+    domains: ['assets.coingecko.com', 'lh3.googleusercontent.com', 'i.ibb.co', 'new.axilthemes.com', 'img.freepik.com', 'encrypted-tbn0.gstatic.com'],
   },
 };
