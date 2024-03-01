c7bd2d1cfdeefdcd34b03e6c36e528587bde379d
Ariful Islam <hello.mrariful@gmail.com>
2024-03-01 15:41:08 +0600

    fully responsive style added with all device and polish full article details page

--- app/dashboard/academy/[_id]/page.jsx
+++ app/dashboard/academy/[_id]/page.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import getArticles from "@/lib/getArticles";
 import Image from "next/image";
-import TextField from "@mui/material/TextField";
+// import TextField from "@mui/material/TextField";
 import moment from "moment";
 import { SiKhanacademy } from "react-icons/si";
 
@@ -17,36 +17,37 @@ const articlePage = async ({ params }) => {
   return (
     <div className="mt-10 container mx-auto">
       <div className="text-white items-center lg:flex gap-2 ml-1">
-       <div className="flex gap-1">
-       <div>
-        <SiKhanacademy className="text-xl" /> 
+        <div className="flex gap-1">
+          <div>
+            <SiKhanacademy className="text-xl" />
+          </div>
+          <div>
+            Academy / {article.category} /
+            <span className="text-blue-400 ml-1">{article.title}</span>
+          </div>
         </div>
-   <div>
-   Academy /  {article.category}  /
-        <span className="text-blue-400 ml-1">{article.title}</span>
-   </div>
-       </div>
       </div>
       <div className="bg-[#1e222d] flex justify-stretch items-center mt-5">
-        <div className="flex justify-between items-center gap-10">
-          <div className="w-1/2 pl-10">
+        <div className="lg:flex justify-between items-center gap-10">
+          <div className="lg:w-1/2 lg:pl-10 px-5 mt-5">
             <button className="bg-blue-500 px-2 py-1 text-sm text-white">
               {article.category}
             </button>
-            <h1 className="xl:text-4xl lg:text-3xl  my-4 font-semibold">{article.title}</h1>
+            <h1 className="xl:text-4xl lg:text-3xl  my-4 font-semibold">
+              {article.title}
+            </h1>
             <div className="">
               <p className="text-[#9d9d9d] lowercase">{tags} |</p>
               <div className="flex">
-              <p className="text-[#9d9d9d]">
-                {moment(article.date).format("ll")} |
-
-              </p>
-              <p className="text-[#9d9d9d] ml-1"> 1k Views</p>
+                <p className="text-[#9d9d9d]">
+                  {moment(article.date).format("ll")} |
+                </p>
+                <p className="text-[#9d9d9d] ml-1"> 1k Views</p>
               </div>
             </div>
           </div>
 
-          <div className="w-1/2 flex justify-end">
+          <div className="lg:w-1/2 mt-5 lg:mt-0 flex justify-end">
             <Image
               src={article.thumbnail}
               alt={article.title}
@@ -62,7 +63,9 @@ const articlePage = async ({ params }) => {
         <h3>{article.description}</h3>
 
         <h1 className="mt-10 text-xl mb-4 text-white">
-          <span className="font-semibold lg:text-3xl text-2xl">Related Topic | </span>{" "}
+          <span className="font-semibold lg:text-3xl text-2xl">
+            Related Topic |{" "}
+          </span>{" "}
           {tagsArray.map((tag, index) => (
             <span
               key={index}
@@ -72,7 +75,7 @@ const articlePage = async ({ params }) => {
             </span>
           ))}
         </h1>
-        <div className="mt-10">
+        {/* <div className="mt-10">
           <h1 className="lg:text-3xl text-2xl font-semibold">Leave A Replay</h1>
           <h3 className="my-3">Your email address will not be published.</h3>
           <TextField
@@ -133,7 +136,7 @@ const articlePage = async ({ params }) => {
           <button className="text-uppercase px-4 py-3 bg-blue-600 text-white my-5">
             Post Comment
           </button>
-        </div>
+        </div> */}
       </div>
     </div>
   );
