29b0630197889d180d979e3459bb02f43d97d8f0
Ariful Islam <hello.mrariful@gmail.com>
2024-03-01 16:02:39 +0600

    uncomment the google translate widget

--- components/home_comp/Translate.jsx
+++ components/home_comp/Translate.jsx
@@ -1,28 +1,28 @@
-// "use client"
-// import { useEffect } from "react";
-// import "./Translate.css"
+"use client"
+import { useEffect } from "react";
+import "./Translate.css"
 
-// const Translate = () => {
-//   const googleTranslateElementInit = () => {
-//     new window.google.translate.TranslateElement(
-//       {
-//         pageLanguage: "en",
-//         autoDisplay: false
-//       },
-//       "google_translate_element"
-//     );
-//   };
-//   useEffect(() => {
-//     let addScript = document.createElement("script");
-//     addScript.setAttribute(
-//       "src",
-//       "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
-//     );
-//     document.body.appendChild(addScript);
-//     window.googleTranslateElementInit = googleTranslateElementInit;
-//   }, []);
+const Translate = () => {
+  const googleTranslateElementInit = () => {
+    new window.google.translate.TranslateElement(
+      {
+        pageLanguage: "en",
+        autoDisplay: false
+      },
+      "google_translate_element"
+    );
+  };
+  useEffect(() => {
+    let addScript = document.createElement("script");
+    addScript.setAttribute(
+      "src",
+      "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
+    );
+    document.body.appendChild(addScript);
+    window.googleTranslateElementInit = googleTranslateElementInit;
+  }, []);
 
-//   return <div id="google_translate_element" />;
-// };
+  return <div id="google_translate_element" />;
+};
 
-// export default Translate;
+export default Translate;
