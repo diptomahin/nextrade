59e34ad772d8a9d9c9106e8fd84a3a7c9b5f18d4
mdnuruzzamannirob <nuruzzamanmd2002@gmail.com>
2024-03-01 21:11:23 +0600

    remove tow routes from the admin

--- components/common/nav_comp/AdminNavLinks.jsx
+++ components/common/nav_comp/AdminNavLinks.jsx
@@ -28,7 +28,7 @@ const AdminNavLinks = ({ pathname }) => {
       >
         <FaUsers className="text-xl" /> Manage Users
       </Link>
-      <Link
+      {/* <Link
         href="/admin_dashboard/manage_services"
         className={`w-full h-10 px-5 btn btn-sm justify-start gap-2 text-white border-2 border-transparent hover:border-transparent hover:border-l-2 hover:border-l-primary rounded-none shadow-none ${
           pathname === "/admin_dashboard/manage_services"
@@ -38,7 +38,7 @@ const AdminNavLinks = ({ pathname }) => {
       >
         <GrServices className="text-xl" />
         Manage Services
-      </Link>
+      </Link> */}
       <Link
         href="/admin_dashboard/manageCoins"
         className={`w-full h-10 px-5 btn btn-sm justify-start gap-2 text-white border-2 border-transparent hover:border-transparent hover:border-l-2 hover:border-l-primary rounded-none shadow-none ${
@@ -60,7 +60,7 @@ const AdminNavLinks = ({ pathname }) => {
       >
         <SiKhanacademy className="text-xl" /> Manage Academy
       </Link>
-      <Link
+      {/* <Link
         href="/admin_dashboard/message_box"
         className={`w-full h-10 px-5 btn btn-sm justify-start gap-2 text-white border-2 border-transparent hover:border-transparent hover:border-l-2 hover:border-l-primary rounded-none shadow-none ${
           pathname === "/admin_dashboard/message_box"
@@ -69,7 +69,7 @@ const AdminNavLinks = ({ pathname }) => {
         }`}
       >
         <LuMessagesSquare className="text-xl" /> Message Box
-      </Link>
+      </Link> */}
     </>
   );
 };
