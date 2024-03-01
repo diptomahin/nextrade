de320f31cb89ead3f3540edcf9e44f791e889ed6
Ariful Islam <hello.mrariful@gmail.com>
2024-03-01 16:14:55 +0600

    modify google translate for resolve the error

---
+++ components/traders_comp/GoogleTranslate/GoogleTranslate.jsx
@@ -0,0 +1,27 @@
+"use client"
+import { useEffect } from "react";
+
+const Translate = () => {
+  const googleTranslateElementInit = () => {
+    new window.google.translate.TranslateElement(
+      {
+        pageLanguage: "en",
+        autoDisplay: false
+      },
+      "google_translate_element"
+    );
+  };
+  useEffect(() => {
+    let addScript = document.createElement("script");
+    addScript.setAttribute(
+      "src",
+      "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
+    );
+    document.body.appendChild(addScript);
+    window.googleTranslateElementInit = googleTranslateElementInit;
+  }, []);
+
+  return <div id="google_translate_element" />;
+};
+
+export default Translate;
