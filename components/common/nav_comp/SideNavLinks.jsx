26cf3b61390112bfcb2c80a53d7db8ddc87a10bc
mdnuruzzamannirob <nuruzzamanmd2002@gmail.com>
2024-03-05 12:08:16 +0600

    upgraded the dashboard sidebar with open close menu button

--- components/common/nav_comp/SideNavLinks.jsx
+++ components/common/nav_comp/SideNavLinks.jsx
@@ -13,91 +13,149 @@ const SideNavLinks = ({ pathname, isActiveMenu }) => {
         className={`w-full h-10 px-5 btn btn-sm justify-start bg-transparent hover:bg-transparent border-4 border-transparent hover:border-transparent hover:border-l-4 hover:border-l-primary rounded-none shadow-none ${
           pathname === "/dashboard"
             ? "text-primary border-l-4 border-l-primary"
-            : "text-[#717579] hover:text-primary"
+            : "text-black hover:text-primary"
         }`}
       >
-        <MdSpaceDashboard className="text-lg" /> Dashboard
+        {isActiveMenu ? (
+          <MdSpaceDashboard className="text-xl" />
+        ) : (
+          <div className="flex items-center gap-2">
+            <MdSpaceDashboard className="text-lg" /> Dashboard
+          </div>
+        )}
       </Link>
       <Link
         href="/dashboard/trading"
         className={`w-full h-10 px-5 btn btn-sm justify-start bg-transparent hover:bg-transparent border-4 border-transparent hover:border-transparent hover:border-l-4 hover:border-l-primary rounded-none shadow-none ${
           pathname === "/dashboard/trading"
             ? "text-primary border-l-4 border-l-primary"
-            : "text-[#717579] hover:text-primary"
-        }`}
+            : "text-black hover:text-primary"
+        } transition-all duration-300 ease-in-out`}
       >
-        <AiFillSliders className="text-lg" />
-        Trading
+        {isActiveMenu ? (
+          <AiFillSliders className="text-xl" />
+        ) : (
+          <div className="flex items-center gap-2">
+            <AiFillSliders className="text-lg" />
+            Trading
+          </div>
+        )}
       </Link>
+
       <Link
         href="/dashboard/market"
         className={`w-full h-10 px-5 btn btn-sm justify-start bg-transparent hover:bg-transparent border-4 border-transparent hover:border-transparent hover:border-l-4 hover:border-l-primary rounded-none shadow-none ${
           pathname === "/dashboard/market"
             ? "text-primary border-l-4 border-l-primary"
-            : "text-[#717579] hover:text-primary"
-        }`}
+            : "text-black hover:text-primary"
+        } transition-all duration-300 ease-in-out`}
       >
-        <SiMarketo className="text-lg" /> Market
+        {isActiveMenu ? (
+          <SiMarketo className="text-xl" />
+        ) : (
+          <div className="flex items-center gap-2">
+            <SiMarketo className="text-lg" />
+            Market
+          </div>
+        )}
       </Link>
+
       <Link
         href="/dashboard/portfolio"
         className={`w-full h-10 px-5 btn btn-sm justify-start bg-transparent hover:bg-transparent border-4 border-transparent hover:border-transparent hover:border-l-4 hover:border-l-primary rounded-none shadow-none ${
           pathname === "/dashboard/portfolio"
             ? "text-primary border-l-4 border-l-primary"
-            : "text-[#717579] hover:text-primary"
-        }`}
+            : "text-black hover:text-primary"
+        } transition-all duration-300 ease-in-out`}
       >
-        <AiOutlineFundView className="text-lg" /> Portfolio
+        {isActiveMenu ? (
+          <AiOutlineFundView className="text-xl" />
+        ) : (
+          <div className="flex items-center gap-2">
+            <AiOutlineFundView className="text-lg" /> Portfolio
+          </div>
+        )}
       </Link>
       <Link
         href="/dashboard/watchlist"
         className={`w-full h-10 px-5 btn btn-sm justify-start bg-transparent hover:bg-transparent border-4 border-transparent hover:border-transparent hover:border-l-4 hover:border-l-primary rounded-none shadow-none ${
           pathname === "/dashboard/watchlist"
             ? "text-primary border-l-4 border-l-primary"
-            : "text-[#717579] hover:text-primary"
-        }`}
+            : "text-black hover:text-primary"
+        } transition-all duration-300 ease-in-out`}
       >
-        <HiMiniQueueList className="text-lg" /> Watchlist
+        {isActiveMenu ? (
+          <HiMiniQueueList className="text-xl" />
+        ) : (
+          <div className="flex items-center gap-2">
+            <HiMiniQueueList className="text-lg" /> Watchlist
+          </div>
+        )}
       </Link>
       <Link
         href="/dashboard/history"
         className={`w-full h-10 px-5 btn btn-sm justify-start bg-transparent hover:bg-transparent border-4 border-transparent hover:border-transparent hover:border-l-4 hover:border-l-primary rounded-none shadow-none ${
           pathname === "/dashboard/history"
             ? "text-primary border-l-4 border-l-primary"
-            : "text-[#717579] hover:text-primary"
-        }`}
+            : "text-black hover:text-primary"
+        } transition-all duration-300 ease-in-out`}
       >
-        <FaHistory className="text-lg" /> History
+        {isActiveMenu ? (
+          <FaHistory className="text-xl" />
+        ) : (
+          <div className="flex items-center gap-2">
+            <FaHistory className="text-lg" /> History
+          </div>
+        )}
       </Link>
       <Link
         href="/dashboard/academy"
         className={`w-full h-10 px-5 btn btn-sm justify-start bg-transparent hover:bg-transparent border-4 border-transparent hover:border-transparent hover:border-l-4 hover:border-l-primary rounded-none shadow-none ${
           pathname === "/dashboard/academy"
             ? "text-primary border-l-4 border-l-primary"
-            : "text-[#717579] hover:text-primary"
-        }`}
+            : "text-black hover:text-primary"
+        } transition-all duration-300 ease-in-out`}
       >
-        <SiKhanacademy className="text-lg" /> Academy
+        {isActiveMenu ? (
+          <SiKhanacademy className="text-xl" />
+        ) : (
+          <div className="flex items-center gap-2">
+            <SiKhanacademy className="text-lg" /> Academy
+          </div>
+        )}
       </Link>
       <Link
         href="/dashboard/profile"
         className={`w-full h-10 px-5 btn btn-sm justify-start bg-transparent hover:bg-transparent border-4 border-transparent hover:border-transparent hover:border-l-4 hover:border-l-primary rounded-none shadow-none ${
           pathname === "/dashboard/profile"
             ? "text-primary border-l-4 border-l-primary"
-            : "text-[#717579] hover:text-primary"
-        }`}
+            : "text-black hover:text-primary"
+        } transition-all duration-300 ease-in-out`}
       >
-        <FaUserCircle className="text-lg" /> Profile
+        {isActiveMenu ? (
+          <FaUserCircle className="text-xl" />
+        ) : (
+          <div className="flex items-center gap-2">
+            <FaUserCircle className="text-lg" />
+            Profile
+          </div>
+        )}
       </Link>
       <Link
         href="/dashboard/wallet"
         className={`w-full h-10 px-5 btn btn-sm justify-start bg-transparent hover:bg-transparent border-4 border-transparent hover:border-transparent hover:border-l-4 hover:border-l-primary rounded-none shadow-none ${
           pathname === "/dashboard/wallet"
             ? "text-primary border-l-4 border-l-primary"
-            : "text-[#717579] hover:text-primary"
-        }`}
+            : "text-black hover:text-primary"
+        } transition-all duration-300 ease-in-out`}
       >
-        <FaWallet className="text-lg" /> Wallet
+        {isActiveMenu ? (
+          <FaWallet className="text-xl transition-all duration-300 ease-in-out" />
+        ) : (
+          <div className="flex items-center gap-2">
+            <FaWallet className="text-lg" /> Wallet
+          </div>
+        )}
       </Link>
     </>
   );
