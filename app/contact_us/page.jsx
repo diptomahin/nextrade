978691ef50b8ec414f59f582f20676b23e30194d
jasaiful <jasaiful@gmail.com>
2024-03-06 07:55:11 +0600

    polish contact us page

--- app/contact_us/page.jsx
+++ app/contact_us/page.jsx
@@ -21,26 +21,26 @@ const page = () => {
         email with short description.
       </Subtitle>
 
-      <div className="mx-auto  3xl:mx-24 bg-[#e9eef1] rounded my-10">
+      <div className="mx-auto  3xl:mx-24 bg-[#1d2334] rounded my-10">
         {/* contact form */}
         <ContactForm></ContactForm>
       </div>
-      <div className="mx-auto mt-10  3xl:mx-24 bg-[#e9eef1] rounded">
-        <div className="flex flex-col md:flex-row justify-around gap-10 py-10">
-          <div className=" 2xl:flex p-10 items-center gap-7 shadow-md lg:p-5  rounded-lg bg-white">
+      <div className="mt-5 3xl:mx-24 bg-gradient-to-br from-darkOne to-darkTwo border border-darkThree hover:bg-gradient-to-tl text-darkGray md:flex justify-between rounded py-10">
+        <div className="flex flex-col md:flex-row justify-between gap-10 py-10">
+          <div className=" 2xl:flex p-10 items-center gap-7 shadow-md lg:p-5  rounded-lg ">
             <div className=" bg-blue-200  rounded-full w-16 h-16  flex justify-center items-center lg:mb-0 mb-3 ">
               <MailOutlinedIcon className=" text-primary" />
             </div>
-            <div className=" flex-1  ">
+            <div className=" flex-1 text-white  ">
               <p className="mt-2">Email:</p>
               <h1 className=" font-bold text-lg">nexttrade.help@gmail.com</h1>
             </div>
           </div>
-          <div className=" 2xl:flex p-10 items-center gap-7 shadow-md lg:p-5  rounded-lg bg-white">
+          <div className=" 2xl:flex p-10 items-center text-white gap-7 shadow-md lg:p-5  rounded-lg">
             <div className=" bg-blue-200  rounded-full w-16 h-16  flex justify-center items-center lg:mb-0 mb-3 ">
               <PhoneForwardedOutlinedIcon className=" text-primary" />
             </div>
-            <div className=" flex-1  ">
+            <div className=" flex-1 text-white ">
               <p className="mt-2">Phone:</p>
               <h1 className=" font-bold text-lg">+880 1963-895488</h1>
             </div>
